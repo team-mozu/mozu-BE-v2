6f6c8f5abaac7b5bdb40965a4584e8eac17c9621
coehgns <modooboiroo@gmail.com>
2025-08-09 16:59:27 +0900

    chore(#9): buildSrc 모듈에 Plugin 추가

---
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -0,0 +1,8 @@
+object Plugin {
+    const val KOTLIN_JVM = "org.jetbrains.kotlin.jvm"
+    const val KOTLIN_SPRING = "org.jetbrains.kotlin.plugin.spring"
+    const val KOTLIN_KAPT = "org.jetbrains.kotlin.kapt"
+    const val SPRING_BOOT = "org.springframework.boot"
+    const val SPRING_DEPENDENCY_MANAGEMENT = "io.spring.dependency-management"
+    const val KTLINT = "org.jlleitschuh.gradle.ktlint"
+}
