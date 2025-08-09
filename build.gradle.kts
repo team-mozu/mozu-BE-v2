99dab685b33e66170416a6a56a24ef20eb1e75dc
coehgns <modooboiroo@gmail.com>
2025-08-09 17:00:24 +0900

    chore(#9): build.gradle.kts에 buildSrc 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.4"
-    id("io.spring.dependency-management") version "1.1.7"
+    id(Plugin.KOTLIN_JVM) version PluginVersions.KOTLIN_VERSION
+    id(Plugin.KOTLIN_SPRING) version PluginVersions.KOTLIN_VERSION
+    id(Plugin.KOTLIN_KAPT) version PluginVersions.KOTLIN_VERSION
+    id(Plugin.SPRING_BOOT) version PluginVersions.SPRING_BOOT_VERSION
+    id(Plugin.SPRING_DEPENDENCY_MANAGEMENT) version PluginVersions.SPRING_DEPENDENCY_MANAGEMENT_VERSION
+    id(Plugin.KTLINT) version PluginVersions.KTLINT_VERSION
 }
 
 group = "mozu"
@@ -19,11 +21,42 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // 스프링 부트 기본 기능
+    implementation(Dependencies.SPRING_BOOT_STARTER)
+
+    // 코틀린 리플렉션
+    implementation(Dependencies.KOTLIN_REFLECT)
+
+    // 스프링 부트 테스트 도구
+    testImplementation(Dependencies.SPRING_BOOT_STARTER_TEST)
+
+    // 코틀린 + JUnit5 테스트
+    testImplementation(Dependencies.KOTLIN_TEST_JUNIT5)
+
+    // JUnit5 실행 런처
+    testRuntimeOnly(Dependencies.JUNIT_PLATFORM_LAUNCHER)
+
+    // 웹 관련
+    implementation(Dependencies.SPRING_BOOT_STARTER_WEB)
+
+    // 데이터베이스
+    implementation(Dependencies.SPRING_BOOT_STARTER_DATA_JPA)
+    implementation(Dependencies.SPRING_BOOT_STARTER_DATA_REDIS)
+    runtimeOnly(Dependencies.MYSQL_CONNECTOR)
+
+    // 보안
+    implementation(Dependencies.SPRING_BOOT_STARTER_SECURITY)
+
+    // 검증
+    implementation(Dependencies.SPRING_BOOT_STARTER_VALIDATION)
+
+    // JSON 처리
+    implementation(Dependencies.JACKSON_MODULE_KOTLIN)
+
+    // JWT
+    implementation(Dependencies.JWT_API)
+    implementation(Dependencies.JWT_IMPL)
+    runtimeOnly(Dependencies.JWT_JACKSON)
 }
 
 kotlin {
