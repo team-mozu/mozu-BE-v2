67091915beff9441cd500fda7008cd062e1b2b0c
박태수 <taesupark0504@gmail.com>
2025-08-14 14:56:16 +0900

    refactor(#16): @Table(name = "tb_admin")으로 리펙토링

--- src/main/kotlin/team/mozu/dsm/infrastructure/persistence/organ/OrganJpaEntity.kt
+++ src/main/kotlin/team/mozu/dsm/infrastructure/persistence/organ/OrganJpaEntity.kt
@@ -2,10 +2,12 @@ package team.mozu.dsm.infrastructure.persistence.organ
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.Table
 import team.mozu.dsm.domain.BaseUUIDEntity
 import java.util.UUID
 
-@Entity(name = "tb_admin")
+@Entity
+@Table(name = "tb_admin") // JPQL은 도메인 모델 네이밍을 유지하고, SQL은 DB 스키마 변경에만 영향을 받도록 설계
 class OrganJpaEntity(
     id: UUID?,
     adminCode: String,
