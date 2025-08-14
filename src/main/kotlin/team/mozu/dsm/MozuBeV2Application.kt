1384cdd88b7048c0cbe9e40d5c17f29b929d5603
박태수 <taesupark0504@gmail.com>
2025-08-14 17:37:24 +0900

    chore(#16): auditing활성화

--- src/main/kotlin/team/mozu/dsm/MozuBeV2Application.kt
+++ src/main/kotlin/team/mozu/dsm/MozuBeV2Application.kt
@@ -2,7 +2,9 @@ package team.mozu.dsm
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing
 @SpringBootApplication
 class MozuBeV2Application
 
