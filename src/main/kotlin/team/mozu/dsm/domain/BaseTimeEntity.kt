36615c32c2083cdba203d00ca1e47220a4c04d05
박태수 <taesupark0504@gmail.com>
2025-08-14 17:37:06 +0900

    feat(#16): BaseTimeEntity

---
+++ src/main/kotlin/team/mozu/dsm/domain/BaseTimeEntity.kt
@@ -0,0 +1,18 @@
+package team.mozu.dsm.domain
+
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseTimeEntity(
+    @CreatedDate
+    private val createdAt: LocalDateTime? = LocalDateTime.now(),
+    @LastModifiedDate
+    private val modifiedAt: LocalDateTime? = LocalDateTime.now()
+) {
+}
