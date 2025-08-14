e6ccd46132e9cae1e1725a784715ff091748a5da
박태수 <taesupark0504@gmail.com>
2025-08-14 13:43:40 +0900

    chore(#16): dms => dsm

--- src/main/kotlin/team/mozu/dsm/MozuBeV2Application.kt
+++ src/main/kotlin/team/mozu/dsm/MozuBeV2Application.kt
@@ -1,4 +1,4 @@
-package team.mozu.dms
+package team.mozu.dsm
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/team/mozu/dsm/MozuBeV2ApplicationTests.kt
+++ src/test/kotlin/team/mozu/dsm/MozuBeV2ApplicationTests.kt
@@ -1,4 +1,4 @@
-package team.mozu.dms
+package team.mozu.dsm
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
