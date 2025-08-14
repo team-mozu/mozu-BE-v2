0b566d5eca1ad8372a9bb92a2a48e3c5c78228ec
박태수 <taesupark0504@gmail.com>
2025-08-14 18:51:12 +0900

    refactor(#16): BaseUUIDEntity 상속 받기

--- src/main/kotlin/team/mozu/dsm/domain/BaseTimeEntity.kt
+++ src/main/kotlin/team/mozu/dsm/domain/BaseTimeEntity.kt
@@ -1,19 +1,27 @@
 package team.mozu.dsm.domain
 
+import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
+import java.util.UUID
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseTimeEntity(
+    id: UUID? = null
+) : BaseUUIDEntity(id) {
     @CreatedDate
-    private val createdAt: LocalDateTime? = LocalDateTime.now(),
-    
+    @Column(updatable = false, nullable = false)
+    var createdAt: LocalDateTime? = null
+        protected set
+
     @LastModifiedDate
-    private val modifiedAt: LocalDateTime? = LocalDateTime.now()
-) {
+    @Column(nullable = false)
+    var modifiedAt: LocalDateTime? = null
+        protected set
+
 }
