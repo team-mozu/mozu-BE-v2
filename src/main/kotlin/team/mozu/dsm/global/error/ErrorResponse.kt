2a7e6eb680552c8b19ef007c0ac63139b5c07ec3
박태수 <taesupark0504@gmail.com>
2025-08-16 00:42:53 +0900

    refactor(#18): httpStatus 적용

--- src/main/kotlin/team/mozu/dsm/global/error/ErrorResponse.kt
+++ src/main/kotlin/team/mozu/dsm/global/error/ErrorResponse.kt
@@ -1,5 +1,6 @@
 package team.mozu.dsm.global.error
 
+import org.springframework.http.HttpStatus
 import team.mozu.dsm.global.error.exception.ErrorCode
 import java.time.LocalDateTime
 
@@ -13,7 +14,15 @@ data class ErrorResponse(
         fun of(errorCode: ErrorCode, description: String?): ErrorResponse =
             ErrorResponse(
                 message = errorCode.message,
-                status = errorCode.status,
+                status = errorCode.httpStatus.value(),   // 여기서 숫자로 변환
+                timestamp = LocalDateTime.now(),
+                description = description
+            )
+
+        fun of(httpStatus: HttpStatus, message: String? = null, description: String? = null): ErrorResponse =
+            ErrorResponse(
+                message = message ?: httpStatus.reasonPhrase,
+                status = httpStatus.value(),
                 timestamp = LocalDateTime.now(),
                 description = description
             )
@@ -26,4 +35,5 @@ data class ErrorResponse(
                 description = description
             )
     }
+
 }
