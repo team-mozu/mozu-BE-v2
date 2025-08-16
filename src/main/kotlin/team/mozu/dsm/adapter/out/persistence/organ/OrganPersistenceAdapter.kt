3e074eff3bf22210e2bf7da5a47b28c3cfeeaf8b
박태수 <taesupark0504@gmail.com>
2025-08-17 01:05:55 +0900

    feat(#25): OrganPersistenceAdapter

---
+++ src/main/kotlin/team/mozu/dsm/adapter/out/persistence/organ/OrganPersistenceAdapter.kt
@@ -0,0 +1,16 @@
+package team.mozu.dsm.adapter.out.persistence.organ
+
+import org.springframework.stereotype.Component
+import team.mozu.dsm.adapter.out.persistence.organ.repository.OrganRepository
+import team.mozu.dsm.application.port.out.FindOrganPort
+import team.mozu.dsm.domain.organ.model.Organ
+
+@Component
+class OrganPersistenceAdapter(
+    private val OrganRepository: OrganRepository,
+    private val organMapper: OrganMapper
+): FindOrganPort {
+    override fun findByOrganCode(organCode: String): Organ? {
+        return OrganRepository.findByOrganCode(organCode)?.let { organMapper.toModel(it) }
+    }
+}
