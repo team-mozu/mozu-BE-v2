f26f406a857bc3372b1701f20a947e66a123df7d
박태수 <taesupark0504@gmail.com>
2025-08-17 01:39:14 +0900

    feat(#25): RefreshTokenRepository

---
+++ src/main/kotlin/team/mozu/dsm/adapter/out/persistence/auth/repository/RefreshTokenRepository.kt
@@ -0,0 +1,7 @@
+package team.mozu.dsm.adapter.out.persistence.auth.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import team.mozu.dsm.adapter.out.auth.entity.RefreshTokenRedisEntity
+
+interface RefreshTokenRepository: JpaRepository<RefreshTokenRedisEntity, String> {
+}
