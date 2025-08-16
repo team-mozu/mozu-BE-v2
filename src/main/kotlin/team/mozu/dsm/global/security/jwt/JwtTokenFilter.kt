6487a8b0ebb3b2d6dd5d1b6ad6b98f26caff5586
박태수 <taesupark0504@gmail.com>
2025-08-17 02:10:59 +0900

    feat(#25): JwtTokenFilter

---
+++ src/main/kotlin/team/mozu/dsm/global/security/jwt/JwtTokenFilter.kt
@@ -0,0 +1,27 @@
+package team.mozu.dsm.global.security.jwt
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+
+class JwtTokenFilter(
+    private val jwtTokenProvider: JwtTokenProvider
+): OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token = jwtTokenProvider.resolveToken(request)
+
+        if (token != null) {
+            // Jwt의 정보를 통해 현재 요청 컨텍스트에 인증 정보 등록
+            val authentication = jwtTokenProvider.getAuthentication(token)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+        filterChain.doFilter(request, response)
+    }
+}
