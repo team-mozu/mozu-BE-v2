cf79f68f83b8111a1042ee79fbcc4a9fb8946252
박태수 <taesupark0504@gmail.com>
2025-08-17 14:08:45 +0900

    refactor(#25): @Repository 어노테이션 제거

--- src/main/kotlin/team/mozu/dsm/adapter/out/persistence/auth/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/persistence/auth/repository/RefreshTokenRepository.kt
@@ -1,8 +1,6 @@
 package team.mozu.dsm.adapter.out.persistence.auth.repository
 
 import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
 import team.mozu.dsm.adapter.out.auth.entity.RefreshTokenRedisEntity
 
-@Repository
 interface RefreshTokenRepository : CrudRepository<RefreshTokenRedisEntity, String>
