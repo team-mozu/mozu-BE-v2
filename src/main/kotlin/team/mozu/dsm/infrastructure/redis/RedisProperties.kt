cd76849c24bf038c631302bd5a633af6bd5df37d
박태수 <taesupark0504@gmail.com>
2025-08-17 14:12:24 +0900

    refactor(#25): RedisProperties 제거

--- src/main/kotlin/team/mozu/dsm/infrastructure/redis/RedisProperties.kt
+++
@@ -1,9 +0,0 @@
-package team.mozu.dsm.infrastructure.redis
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "spring.data.redis")
-data class RedisProperties(
-    val host: String,
-    val port: Int
-)
