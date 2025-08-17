e26eea4903b16b3847b55edb4bb1af796ebaae88
박태수 <taesupark0504@gmail.com>
2025-08-17 10:34:04 +0900

    refactor(#25): OrganNotFoundException object 클래스로 변경

--- src/main/kotlin/team/mozu/dsm/infrastructure/exception/organ/OrganNotFoundException.kt
+++ src/main/kotlin/team/mozu/dsm/infrastructure/exception/organ/OrganNotFoundException.kt
@@ -3,10 +3,5 @@ package team.mozu.dsm.infrastructure.exception.organ
 import team.mozu.dsm.global.error.exception.ErrorCode
 import team.mozu.dsm.global.error.exception.MozuException
 
-class OrganNotFoundException : MozuException
-(ErrorCode.ORGAN_NOT_FOUND) {
-    companion object {
-        @JvmField
-        val EXCEPTION = OrganNotFoundException()
-    }
+object OrganNotFoundException: MozuException(ErrorCode.ORGAN_NOT_FOUND) {
 }
