2b35b6c3b47d5668ece38c1f0acb8b2ea2d3ba6e
박태수 <taesupark0504@gmail.com>
2025-08-17 15:55:44 +0900

    refactor(#25): 구체적인 import로 변경

--- src/main/kotlin/team/mozu/dsm/adapter/out/organ/persistence/repository/OrganRepository.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/organ/persistence/repository/OrganRepository.kt
@@ -2,7 +2,7 @@ package team.mozu.dsm.adapter.out.organ.persistence.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import team.mozu.dsm.adapter.out.organ.entity.OrganJpaEntity
-import java.util.*
+import java.util.UUID
 
 interface OrganRepository : JpaRepository<OrganJpaEntity, UUID> {
     fun findByOrganCode(organCode: String): OrganJpaEntity?
