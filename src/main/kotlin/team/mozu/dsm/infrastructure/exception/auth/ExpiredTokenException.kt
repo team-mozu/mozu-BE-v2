755bb67a502a9d6ae201ae73298d4005141c5ab2
박태수 <taesupark0504@gmail.com>
2025-08-17 10:31:06 +0900

    refactor(#25): ExpiredTokenException object클래스로 변경

--- src/main/kotlin/team/mozu/dsm/infrastructure/exception/auth/ExpiredTokenException.kt
+++ src/main/kotlin/team/mozu/dsm/infrastructure/exception/auth/ExpiredTokenException.kt
@@ -3,9 +3,4 @@ package team.mozu.dsm.infrastructure.exception.auth
 import team.mozu.dsm.global.error.exception.ErrorCode
 import team.mozu.dsm.global.error.exception.MozuException
 
-class ExpiredTokenException : MozuException
-(ErrorCode.EXPIRED_TOKEN) {
-    companion object {
-        val EXCEPTION = ExpiredTokenException()
-    }
-}
+object ExpiredTokenException: MozuException(ErrorCode.EXPIRED_TOKEN)
