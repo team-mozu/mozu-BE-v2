2afd5f2a0cfafc042d6b00e0d03ef028178c3dd5
박태수 <taesupark0504@gmail.com>
2025-08-17 13:57:25 +0900

    refactor(#25): ConfigurationPropertiesScan Config로 관리

--- src/main/kotlin/team/mozu/dsm/MozuBeV2Application.kt
+++ src/main/kotlin/team/mozu/dsm/MozuBeV2Application.kt
@@ -1,13 +1,11 @@
 package team.mozu.dsm
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableJpaAuditing
 @SpringBootApplication
-@ConfigurationPropertiesScan
 class MozuBeV2Application
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/team/mozu/dsm/infrastructure/config/security/ConfigurationPropertiesConfig.kt
@@ -0,0 +1,8 @@
+package team.mozu.dsm.infrastructure.config.security
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationPropertiesScan
+class ConfigurationPropertiesConfig
