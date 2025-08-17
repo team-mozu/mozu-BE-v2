50856e7f192bc6037822744048750dbe2d8ce285
박태수 <taesupark0504@gmail.com>
2025-08-17 13:51:35 +0900

    refactor(#25): 반환형 UserDetails로 맞추기

--- src/main/kotlin/team/mozu/dsm/global/security/auth/CustomUserDetailsService.kt
+++ src/main/kotlin/team/mozu/dsm/global/security/auth/CustomUserDetailsService.kt
@@ -1,5 +1,6 @@
 package team.mozu.dsm.global.security.auth
 
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Component
@@ -9,7 +10,7 @@ import team.mozu.dsm.application.port.out.FindOrganPort
 class CustomUserDetailsService(
     private val findOrganPort: FindOrganPort
 ) : UserDetailsService {
-    override fun loadUserByUsername(organCode: String): CustomUserDetails {
+    override fun loadUserByUsername(organCode: String): UserDetails {
         val organ = findOrganPort.findByOrganCode(organCode) ?: throw UsernameNotFoundException("Organ Not Found")
         return CustomUserDetails(organ)
     }
