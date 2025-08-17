a70aa67b07e6cd08976840aac324fcd58e09a04f
박태수 <taesupark0504@gmail.com>
2025-08-17 10:31:31 +0900

    refactor(#25): InvalidTokenException object클래스로 변경

--- src/main/kotlin/team/mozu/dsm/infrastructure/exception/auth/InvalidTokenException.kt
+++ src/main/kotlin/team/mozu/dsm/infrastructure/exception/auth/InvalidTokenException.kt
@@ -3,9 +3,4 @@ package team.mozu.dsm.infrastructure.exception.auth
 import team.mozu.dsm.global.error.exception.ErrorCode
 import team.mozu.dsm.global.error.exception.MozuException
 
-class InvalidTokenException : MozuException
-(ErrorCode.INVALID_TOKEN) {
-    companion object {
-        val EXCEPTION = InvalidTokenException()
-    }
-}
+object InvalidTokenException: MozuException(ErrorCode.INVALID_TOKEN)
