3cb8286bd088d6a72f74eb84c01ae91f3e0b6624
민수아 <Minsua0503@gmail.com>
2025-08-18 11:43:08 +0900

    feat(#27): ClassMapper

---
+++ src/main/kotlin/team/mozu/dsm/adapter/out/class/persistence/mapper/ClassMapper.kt
@@ -0,0 +1,47 @@
+package team.mozu.dsm.adapter.out.`class`.persistence.mapper
+
+import org.springframework.stereotype.Component
+import team.mozu.dsm.adapter.out.`class`.entity.ClassJpaEntity
+import team.mozu.dsm.adapter.out.organ.persistence.repository.OrganRepository
+import team.mozu.dsm.application.exception.organ.OrganNotFoundException
+import team.mozu.dsm.domain.`class`.model.Class
+
+@Component
+class ClassMapper(
+    private val organRepository: OrganRepository
+) {
+
+    fun toModel(entity: ClassJpaEntity): Class {
+        return Class(
+            id = entity.id,
+            organId = entity.organ.id!!,
+            className = entity.className,
+            maxInvRound = entity.maxInvRound,
+            curInvRound = entity.curInvRound,
+            baseMoney = entity.baseMoney,
+            classNum = entity.classNum,
+            isStarred = entity.isStarred,
+            isDeleted = entity.isDeleted,
+            isInProgress = entity.isInProgress,
+            createdAt = entity.createdAt,
+            updatedAt = entity.updatedAt
+        )
+    }
+
+    fun toEntity(model: Class): ClassJpaEntity {
+        val organEntity = organRepository.findById(model.organId)
+            .orElseThrow{ OrganNotFoundException }
+
+        return ClassJpaEntity(
+            organ = organEntity,
+            className = model.className,
+            maxInvRound = model.maxInvRound,
+            curInvRound = model.curInvRound,
+            baseMoney = model.baseMoney,
+            classNum = model.classNum,
+            isStarred = model.isStarred,
+            isDeleted = model.isDeleted,
+            isInProgress = model.isInProgress
+        )
+    }
+}
