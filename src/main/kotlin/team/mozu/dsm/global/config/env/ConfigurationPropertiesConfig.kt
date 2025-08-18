c81b8f39835771c1993a48eb8672a5579da9c327
민수아 <Minsua0503@gmail.com>
2025-08-18 11:43:40 +0900

    refactor(#27): 패키지 경로 global/config -> global/config/env 하위 패키지로 이동

--- src/main/kotlin/team/mozu/dsm/global/config/env/ConfigurationPropertiesConfig.kt
+++ src/main/kotlin/team/mozu/dsm/global/config/env/ConfigurationPropertiesConfig.kt
@@ -1,4 +1,4 @@
-package team.mozu.dsm.global.config
+package team.mozu.dsm.global.config.env
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
