3cc4ad42d1df4f87995dac6d0c9bb46fa9988b4a
imhanseong <hans_hanseong@dsm.hs.kr>
2025-08-21 01:23:13 +0900

    feat(#34): Item

---
+++ src/main/kotlin/team/mozu/dsm/domain/item/model/Item.kt
@@ -0,0 +1,23 @@
+package team.mozu.dsm.domain.item.model
+
+import team.mozu.dsm.domain.annotation.Aggregate
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Aggregate
+data class Item (
+    val id : UUID?,
+    val organId : UUID,
+    val itemName : String,
+    val itemLogo : String,
+    val itemInfo : String,
+    val createdAt : LocalDateTime?,
+    val money : Int,
+    val debt : Int,
+    val capital : Int,
+    val profit : Int,
+    val profitOg : Int,
+    val profitBenefit : Int,
+    val netProfit : Int,
+    val isDeleted : Boolean
+)
