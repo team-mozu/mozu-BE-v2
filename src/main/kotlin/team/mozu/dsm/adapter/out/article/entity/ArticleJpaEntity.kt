0722dfa75d3fe010bffb9fbf99a10080db9fe405
imhanseong <hans_hanseong@dsm.hs.kr>
2025-08-21 00:23:35 +0900

    feat(#32): ArticleJpaEntity

---
+++ src/main/kotlin/team/mozu/dsm/adapter/out/article/entity/ArticleJpaEntity.kt
@@ -0,0 +1,26 @@
+package team.mozu.dsm.adapter.out.article.entity
+
+import jakarta.persistence.*
+import team.mozu.dsm.adapter.out.organ.entity.OrganJpaEntity
+import team.mozu.dsm.global.entity.BaseTimeEntity
+
+@Entity
+@Table(name = "tbl_article")
+class ArticleJpaEntity (
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(300)")
+    var articleName: String,
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(10000)")
+    var articleDescription: String,
+
+    @Column(nullable = true, columnDefinition = "VARCHAR(255)")
+    var articleImage: String,
+
+    @Column(nullable = false)
+    var isDeleted: Boolean = false,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organ_id", nullable = false)
+    var organ: OrganJpaEntity
+) : BaseTimeEntity()
