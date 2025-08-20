660a69cbfea7b6b939f7870acd0246ee00a82cf4
imhanseong <hans_hanseong@dsm.hs.kr>
2025-08-21 00:23:50 +0900

    feat(#32): Article

---
+++ src/main/kotlin/team/mozu/dsm/domain/article/model/Article.kt
@@ -0,0 +1,17 @@
+package team.mozu.dsm.domain.article.model
+
+import team.mozu.dsm.domain.annotation.Aggregate
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Aggregate
+data class Article (
+    val id : UUID?,
+    val organId : UUID,
+    val articleName : String,
+    val articleDescription : String,
+    val articleImage : String,
+    val createDate : LocalDateTime,
+    val isDeleted : Boolean
+)
+
