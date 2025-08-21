dcffcdd773ddc74bcc878f3e7843e3e9ffb66b48
imhanseong <hans_hanseong@dsm.hs.kr>
2025-08-21 23:22:26 +0900

    refactor(#34): ItemRepository 수정

--- src/main/kotlin/team/mozu/dsm/adapter/out/item/persistence/repository/ItemRepository.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/item/persistence/repository/ItemRepository.kt
@@ -4,4 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import team.mozu.dsm.adapter.out.`class`.entity.ClassJpaEntity
 import java.util.UUID
 
-interface ItemRepository : JpaRepository<ClassJpaEntity, UUID>
+interface ItemRepository : JpaRepository<ItemRepository, UUID>
