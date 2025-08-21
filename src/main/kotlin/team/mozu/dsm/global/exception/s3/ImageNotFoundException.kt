1c0c5cecf1775c7c7a72d3459b9cdaee526079ba
박태수 <taesupark0504@gmail.com>
2025-08-21 23:08:49 +0900

    refactor(#37): {} 제거

--- src/main/kotlin/team/mozu/dsm/global/exception/s3/ImageNotFoundException.kt
+++ src/main/kotlin/team/mozu/dsm/global/exception/s3/ImageNotFoundException.kt
@@ -3,5 +3,4 @@ package team.mozu.dsm.global.exception.s3
 import team.mozu.dsm.global.error.exception.ErrorCode
 import team.mozu.dsm.global.error.exception.MozuException
 
-object ImageNotFoundException : MozuException(ErrorCode.IMAGE_NOT_FOUND) {
-}
+object ImageNotFoundException : MozuException(ErrorCode.IMAGE_NOT_FOUND)
