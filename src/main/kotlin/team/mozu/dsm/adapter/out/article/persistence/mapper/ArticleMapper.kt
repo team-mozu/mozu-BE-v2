2a59bb3e9a233b5d7b14e622cba8c5e0b23108e8
imhanseong <hans_hanseong@dsm.hs.kr>
2025-08-21 23:08:32 +0900

    refactor(#32): ArticleMapper 수정

--- src/main/kotlin/team/mozu/dsm/adapter/out/article/persistence/mapper/ArticleMapper.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/article/persistence/mapper/ArticleMapper.kt
@@ -1,32 +1,12 @@
 package team.mozu.dsm.adapter.out.article.persistence.mapper
 
-import org.springframework.stereotype.Component
+import org.mapstruct.Mapper
 import team.mozu.dsm.adapter.out.article.entity.ArticleJpaEntity
 import team.mozu.dsm.adapter.out.organ.entity.OrganJpaEntity
 import team.mozu.dsm.domain.article.model.Article
 
-@Component
-class ArticleMapper {
-
-    fun toModel(entity: ArticleJpaEntity): Article {
-        return Article(
-            id = entity.id,
-            organId = entity.organ.id!!,
-            articleName =  entity.articleName,
-            articleDescription = entity.articleDescription,
-            articleImage = entity.articleImage,
-            isDeleted = entity.isDeleted,
-            createAt = entity.createdAt
-        )
-    }
-
-    fun toEntity(model: Article, organ: OrganJpaEntity): ArticleJpaEntity {
-        return ArticleJpaEntity(
-            organ = organ,
-            articleName = model.articleName,
-            articleDescription = model.articleDescription,
-            articleImage = model.articleImage,
-            isDeleted = model.isDeleted
-        )
-    }
+@Mapper(componentModel = "spring")
+interface ArticleMapper {
+    fun toModel(entity: ArticleJpaEntity): Article
+    fun toEntity(model: Article, organ: OrganJpaEntity): ArticleJpaEntity
 }
