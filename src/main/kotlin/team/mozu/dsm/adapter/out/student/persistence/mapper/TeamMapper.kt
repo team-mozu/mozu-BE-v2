d31183d3b63780c3220c5d2b3b6705ec74591026
박지민 <pjmlove2008@gmail.com>
2025-08-22 13:50:42 +0900

    refactor(#36): TeamMapper 수정

--- src/main/kotlin/team/mozu/dsm/adapter/out/student/persistence/mapper/TeamMapper.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/student/persistence/mapper/TeamMapper.kt
@@ -1,41 +1,16 @@
 package team.mozu.dsm.adapter.out.student.persistence.mapper
 
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import team.mozu.dsm.adapter.out.`class`.entity.ClassJpaEntity
 import team.mozu.dsm.adapter.out.student.entity.TeamJpaEntity
 import team.mozu.dsm.domain.student.model.Team
 
 @Mapper(componentModel = "spring")
-class TeamMapper {
+interface TeamMapper {
 
-    fun toModel(entity: TeamJpaEntity): Team {
-        return Team(
-            id = entity.id,
-            classId = entity.`class`.id!!,
-            teamName = entity.teamName,
-            schoolName = entity.schoolName,
-            totalMoney = entity.totalMoney,
-            cashMoney = entity.cashMoney,
-            valuationMoney = entity.valuationMoney,
-            classNumber = entity.classNumber,
-            investmentCompleteYn = entity.investmentCompleteYn,
-            participationDate = entity.participationDate,
-            createdAt = entity.createdAt,
-            updatedAt = entity.updatedAt
-        )
-    }
+    @Mapping(target = "classId", source = "class.id")
+    fun toModel(entity: TeamJpaEntity): Team
 
-    fun toEntity(model: Team, `class`: ClassJpaEntity): TeamJpaEntity {
-        return TeamJpaEntity(
-            `class` = `class`,
-            teamName = model.teamName,
-            schoolName = model.schoolName,
-            totalMoney = model.totalMoney,
-            cashMoney = model.cashMoney,
-            valuationMoney = model.valuationMoney,
-            classNumber = model.classNumber,
-            investmentCompleteYn = model.investmentCompleteYn,
-            participationDate = model.participationDate
-        )
-    }
+    fun toEntity(model: Team, `class`: ClassJpaEntity): TeamJpaEntity
 }
