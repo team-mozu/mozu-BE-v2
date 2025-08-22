437b76b1593f055092ee7711a694071aa97e18b6
박지민 <pjmlove2008@gmail.com>
2025-08-22 11:53:15 +0900

    feat(#36): TeamJpaEntity

---
+++ src/main/kotlin/team/mozu/dsm/adapter/out/student/entity/TeamJpaEntity.kt
@@ -0,0 +1,45 @@
+package team.mozu.dsm.adapter.out.student.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import team.mozu.dsm.adapter.out.`class`.entity.ClassJpaEntity
+import team.mozu.dsm.global.entity.BaseTimeEntity
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "tbl_class_team")
+class TeamJpaEntity (
+
+    @Column(name = "team_name", columnDefinition = "VARCHAR(100)")
+    var teamName: String?,
+
+    @Column(name = "school_name", nullable = false, columnDefinition = "VARCHAR(100)")
+    var schoolName: String,
+
+    @Column(name = "total_money", nullable = false)
+    var totalMoney: Int,
+
+    @Column(name = "cash_money", nullable = false)
+    var cashMoney: Int,
+
+    @Column(name = "valuation_money", nullable = false)
+    var valuationMoney: Int,
+
+    @Column(name = "class_number", nullable = false, columnDefinition = "VARCHAR(40)")
+    var classNumber: String,
+
+    @Column(name = "investment_complete_yn", nullable = false)
+    var investmentCompleteYn: Boolean = false,
+
+    @Column(name = "participation_date", nullable = false)
+    var participationDate: LocalDateTime,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_id", nullable = false)
+    var `class` : ClassJpaEntity
+
+) : BaseTimeEntity()
