2d215072661ddca7772de9b35467fdd06a14b7f4
박지민 <pjmlove2008@gmail.com>
2025-08-23 22:33:13 +0900

    fix(#41): ItemMapper에서 MapStruct 매핑 충돌 해결

--- src/main/kotlin/team/mozu/dsm/adapter/out/item/persistence/mapper/ItemMapper.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/item/persistence/mapper/ItemMapper.kt
@@ -6,7 +6,23 @@ import team.mozu.dsm.adapter.out.organ.entity.OrganJpaEntity
 import team.mozu.dsm.domain.item.model.Item
 
 @Mapper(componentModel = "spring")
-interface ItemMapper {
-    fun toModel(entity: ItemJpaEntity): Item
-    fun toEntity(model: Item, organ: OrganJpaEntity): ItemJpaEntity
+abstract class ItemMapper {
+    abstract fun toModel(entity: ItemJpaEntity): Item
+
+    fun toEntity(model: Item, organ: OrganJpaEntity): ItemJpaEntity {
+        return ItemJpaEntity(
+            organ = organ,
+            itemName = model.itemName,
+            itemLogo = model.itemLogo,
+            itemInfo = model.itemInfo,
+            isDeleted = model.isDeleted,
+            capital = model.capital,
+            debt = model.debt,
+            profit = model.profit,
+            profitOg = model.profitOg,
+            profitBenefit = model.profitBenefit,
+            netProfit = model.netProfit,
+            money = model.money
+        )
+    }
 }
