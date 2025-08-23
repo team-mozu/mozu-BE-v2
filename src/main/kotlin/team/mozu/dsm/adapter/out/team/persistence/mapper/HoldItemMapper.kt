58df8d6ae242d4dd60692d5518e7f7f5d9935d5b
박지민 <pjmlove2008@gmail.com>
2025-08-23 22:36:52 +0900

    feat(#41): HoldItemMapper

---
+++ src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/mapper/HoldItemMapper.kt
@@ -0,0 +1,23 @@
+package team.mozu.dsm.adapter.out.team.persistence.mapper
+
+import org.mapstruct.Mapper
+import team.mozu.dsm.adapter.out.team.entity.HoldItemJpaEntity
+import team.mozu.dsm.adapter.out.team.entity.TeamJpaEntity
+import team.mozu.dsm.domain.team.model.HoldItem
+
+@Mapper(componentModel = "spring")
+abstract class HoldItemMapper {
+
+    abstract fun toModel(entity: HoldItemJpaEntity): HoldItem
+
+    fun toEntity(model: HoldItem, team: TeamJpaEntity): HoldItemJpaEntity {
+        return HoldItemJpaEntity(
+            team = team,
+            itemName = model.itemName,
+            itemCount = model.itemCount,
+            buyMoney = model.buyMoney,
+            valProfit = model.valProfit,
+            profitNum = model.profitNum
+        )
+    }
+}
