6abbcce085ce0b85354251240ea2e3906a36190a
박지민 <pjmlove2008@gmail.com>
2025-08-23 23:06:23 +0900

    feat(#41): HoldItemMapper toModel에 teamId 매핑 추가

--- src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/mapper/HoldItemMapper.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/mapper/HoldItemMapper.kt
@@ -1,6 +1,7 @@
 package team.mozu.dsm.adapter.out.team.persistence.mapper
 
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import team.mozu.dsm.adapter.out.team.entity.HoldItemJpaEntity
 import team.mozu.dsm.adapter.out.team.entity.TeamJpaEntity
 import team.mozu.dsm.domain.team.model.HoldItem
@@ -8,6 +9,7 @@ import team.mozu.dsm.domain.team.model.HoldItem
 @Mapper(componentModel = "spring")
 abstract class HoldItemMapper {
 
+    @Mapping(target = "teamId", source = "team.id")
     abstract fun toModel(entity: HoldItemJpaEntity): HoldItem
 
     fun toEntity(model: HoldItem, team: TeamJpaEntity): HoldItemJpaEntity {
