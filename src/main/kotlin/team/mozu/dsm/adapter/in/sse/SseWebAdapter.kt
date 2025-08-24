b8e3448d320c89be511389e21a93cc884f0ff31a
박태수 <taesupark0504@gmail.com>
2025-08-24 16:00:34 +0900

    feat(#40): SseWebAdapter sse 전역 연결 요청 api

---
+++ src/main/kotlin/team/mozu/dsm/adapter/in/sse/SseWebAdapter.kt
@@ -0,0 +1,21 @@
+package team.mozu.dsm.adapter.`in`.sse
+
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+import team.mozu.dsm.application.port.`in`.sse.SseUseCase
+
+@RestController
+@RequestMapping("/sse")
+class SseWebAdapter(
+    private val sseUseCase: SseUseCase
+) {
+
+    @GetMapping("/subscribe")
+    fun subscribe(@RequestBody @Valid clientId: String): SseEmitter {
+        return sseUseCase.subscribe(clientId)
+    }
+}
