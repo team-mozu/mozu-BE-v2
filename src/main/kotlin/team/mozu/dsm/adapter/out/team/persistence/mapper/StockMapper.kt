6212012524bdb80d0fd2fc4dd3d6865ac23b3fe2
박지민 <pjmlove2008@gmail.com>
2025-08-24 18:05:57 +0900

    feat(#41): toModel에 상속 필드 @Mapping 명시

--- src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/mapper/StockMapper.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/mapper/StockMapper.kt
@@ -9,9 +9,20 @@ import team.mozu.dsm.domain.team.model.Stock
 @Mapper(componentModel = "spring")
 interface StockMapper {
 
+    /**
+     * 상속 필드는 Kotlin 생성자 매핑 시 자동 연결되지 않을 수 있으므로
+     * 명시적으로 @Mapping을 지정함
+     */
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "createdAt", source = "createdAt")
+    @Mapping(target = "updatedAt", source = "updatedAt")
     @Mapping(target = "teamId", source = "team.id")
     fun toModel(entity: StockJpaEntity): Stock
 
+    /**
+     * MapStruct는 DB 조회를 지원하지 않으므로 toEntity 메서드는 수동 구현 방식을 사용함
+     * (StockJpaEntity에는 TeamJpaEntity 객체 필드만 존재 -> teamId를 엔티티에서 찾으려면 DB 조회가 필요)
+     */
     fun toEntity(model: Stock, team: TeamJpaEntity): StockJpaEntity {
         return StockJpaEntity(
             team = team,
@@ -23,8 +34,3 @@ interface StockMapper {
         )
     }
 }
-
-/*
-MapStruct는 DB 조회를 지원하지 않으므로 toEntity 메서드는 수동 구현 방식을 사용하였습니다
-(StockJpaEntity에는 TeamJpaEntity 객체 필드만 존재 -> teamId를 엔티티에서 찾으려면 DB 조회가 필요)
- */
