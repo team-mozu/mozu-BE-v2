5667661ede72fa3a63d0ef2bd07601b630ef07de
박태수 <taesupark0504@gmail.com>
2025-08-24 23:24:10 +0900

    refactor(#40): publish메서드 추가

--- src/main/kotlin/team/mozu/dsm/application/port/in/sse/SseUseCase.kt
+++ src/main/kotlin/team/mozu/dsm/application/port/in/sse/SseUseCase.kt
@@ -5,4 +5,8 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 interface SseUseCase {
 
     fun subscribe(clientId: String): SseEmitter
+
+    fun publishTo(clientId: String, eventName: String, data: Any)
+
+    fun publishToAll(eventName: String, data: Any)
 }
