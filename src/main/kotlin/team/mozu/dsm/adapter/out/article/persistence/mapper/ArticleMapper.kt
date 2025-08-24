086a8b40662efb5475118bff67fdc3f22d25c170
민수아 <Minsua0503@gmail.com>
2025-08-24 14:18:49 +0900

    fix(#43): toEntity id 충돌문제 해결

--- src/main/kotlin/team/mozu/dsm/adapter/out/article/persistence/mapper/ArticleMapper.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/article/persistence/mapper/ArticleMapper.kt
@@ -1,12 +1,16 @@
 package team.mozu.dsm.adapter.out.article.persistence.mapper
 
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import team.mozu.dsm.adapter.out.article.entity.ArticleJpaEntity
 import team.mozu.dsm.adapter.out.organ.entity.OrganJpaEntity
 import team.mozu.dsm.domain.article.model.Article
 
 @Mapper(componentModel = "spring")
 interface ArticleMapper {
+
     fun toModel(entity: ArticleJpaEntity): Article
+
+    @Mapping(target = "id", source = "model.id")
     fun toEntity(model: Article, organ: OrganJpaEntity): ArticleJpaEntity
 }
--- src/main/kotlin/team/mozu/dsm/adapter/out/item/persistence/mapper/ItemMapper.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/item/persistence/mapper/ItemMapper.kt
@@ -1,12 +1,16 @@
 package team.mozu.dsm.adapter.out.item.persistence.mapper
 
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import team.mozu.dsm.adapter.out.item.entity.ItemJpaEntity
 import team.mozu.dsm.adapter.out.organ.entity.OrganJpaEntity
 import team.mozu.dsm.domain.item.model.Item
 
 @Mapper(componentModel = "spring")
 interface ItemMapper {
+
     fun toModel(entity: ItemJpaEntity): Item
+
+    @Mapping(target = "id", source = "model.id")
     fun toEntity(model: Item, organ: OrganJpaEntity): ItemJpaEntity
 }
