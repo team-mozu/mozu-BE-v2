325463ab972acff6575e79e3d352d50fcc0f58c1
박태수 <taesupark0504@gmail.com>
2025-08-24 14:15:47 +0900

    feat(#40): SseUseCase

---
+++ src/main/kotlin/team/mozu/dsm/application/port/in/sse/SseUseCase.kt
@@ -0,0 +1,8 @@
+package team.mozu.dsm.application.port.`in`.sse
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+
+interface SseUseCase {
+
+    fun subscribe(clientId: String): SseEmitter
+}
