0884ac14926f10fef3da8255bfc6348825f31e9a
박지민 <pjmlove2008@gmail.com>
2025-08-26 05:43:33 +0900

    fix(#56): toModel 수동 구현

--- src/main/kotlin/team/mozu/dsm/adapter/out/lesson/persistence/mapper/LessonMapper.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/lesson/persistence/mapper/LessonMapper.kt
@@ -1,7 +1,6 @@
 package team.mozu.dsm.adapter.out.lesson.persistence.mapper
 
 import org.mapstruct.Mapper
-import org.mapstruct.Mapping
 import team.mozu.dsm.adapter.out.lesson.entity.LessonJpaEntity
 import team.mozu.dsm.adapter.out.organ.entity.OrganJpaEntity
 import team.mozu.dsm.domain.lesson.model.Lesson
@@ -9,8 +8,27 @@ import team.mozu.dsm.domain.lesson.model.Lesson
 @Mapper(componentModel = "spring")
 abstract class LessonMapper {
 
-    @Mapping(target = "organId", source = "organ.id")
-    abstract fun toModel(entity: LessonJpaEntity): Lesson
+    /**
+     * JpaEntity의 Boolean 필드는 컴파일 시 is 접두사가 제거됨
+     * 필드명 불일치 -> 자동 매핑 안됨 = 기본값인 false 할당
+     * 기본 값 할당을 하기에 기능 구현이 어려워 수동 구현을 함
+     */
+    fun toModel(entity: LessonJpaEntity): Lesson {
+        return Lesson(
+            id = entity.id,
+            organId = entity.organ.id!!,
+            lessonName = entity.lessonName,
+            maxInvRound = entity.maxInvRound,
+            curInvRound = entity.curInvRound,
+            baseMoney = entity.baseMoney,
+            lessonNum = entity.lessonNum,
+            isStarred = entity.isStarred,
+            isDeleted = entity.isDeleted,
+            isInProgress = entity.isInProgress,
+            createdAt = entity.createdAt!!,
+            updatedAt = entity.updatedAt
+        )
+    }
 
     fun toEntity(model: Lesson, organ: OrganJpaEntity): LessonJpaEntity {
         return LessonJpaEntity(
