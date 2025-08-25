3a67482672f4cc4ca6047317f1f60a110d252e42
민수아 <Minsua0503@gmail.com>
2025-08-25 12:56:28 +0900

    feat(#47): LessonItemId

---
+++ src/main/kotlin/team/mozu/dsm/adapter/out/lesson/entity/id/LessonItemId.kt
@@ -0,0 +1,11 @@
+package team.mozu.dsm.adapter.out.lesson.entity.id
+
+import jakarta.persistence.Embeddable
+import java.io.Serializable
+import java.util.UUID
+
+@Embeddable
+data class LessonItemId (
+    var lessonId: UUID,
+    var itemId: UUID
+): Serializable
