a7e41bc4f2521dc9338249734e1b2bcb2fbb65b7
박지민 <pjmlove2008@gmail.com>
2025-08-26 06:30:50 +0900

    feat(#56): 유효성 검증 추가

--- src/main/kotlin/team/mozu/dsm/adapter/in/team/dto/request/TeamParticipationRequest.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/in/team/dto/request/TeamParticipationRequest.kt
@@ -4,10 +4,10 @@ import jakarta.validation.constraints.NotBlank
 
 data class TeamParticipationRequest(
 
-    @field:NotBlank(message = "lessonNum은 비어있을 수 없습니다.")
+    @field:NotBlank(message = "lessonNum은 필수입니다")
     val lessonNum: String,
-    @field:NotBlank(message = "schoolName은 비어있을 수 없습니다.")
+    @field:NotBlank(message = "schoolName은 필수입니다")
     val schoolName: String,
-    @field:NotBlank(message = "teamName은 비어있을 수 없습니다.")
+    @field:NotBlank(message = "teamName은 필수입니다")
     val teamName: String
 )
