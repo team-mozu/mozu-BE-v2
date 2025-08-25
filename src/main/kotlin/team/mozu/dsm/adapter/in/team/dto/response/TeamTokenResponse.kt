e44052e3452cc337aedb751bfe5545ce391e67bb
박지민 <pjmlove2008@gmail.com>
2025-08-25 15:10:49 +0900

    style(#48): ktlint 적용

--- src/main/kotlin/team/mozu/dsm/adapter/in/team/dto/response/TeamTokenResponse.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/in/team/dto/response/TeamTokenResponse.kt
@@ -2,7 +2,7 @@ package team.mozu.dsm.adapter.`in`.team.dto.response
 
 import java.time.LocalDateTime
 
-data class TeamTokenResponse (
+data class TeamTokenResponse(
     val accessToken: String,
     val accessExpiredAt: LocalDateTime
 )
