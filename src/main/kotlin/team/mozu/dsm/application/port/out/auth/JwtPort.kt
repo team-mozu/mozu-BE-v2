dea84bffd009c897697ab6396e81c7e29ba3b583
박지민 <pjmlove2008@gmail.com>
2025-08-25 14:48:24 +0900

    feat(#48): createStudentAccessToken 추가

--- src/main/kotlin/team/mozu/dsm/application/port/out/auth/JwtPort.kt
+++ src/main/kotlin/team/mozu/dsm/application/port/out/auth/JwtPort.kt
@@ -3,6 +3,7 @@ package team.mozu.dsm.application.port.out.auth
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.security.core.Authentication
 import team.mozu.dsm.adapter.`in`.auth.dto.response.TokenResponse
+import team.mozu.dsm.adapter.`in`.team.dto.response.TeamTokenResponse
 
 interface JwtPort {
 
@@ -11,4 +12,6 @@ interface JwtPort {
     fun getAuthentication(token: String): Authentication
 
     fun resolveToken(request: HttpServletRequest): String?
+
+    fun createStudentAccessToken(lessonNum: String): TeamTokenResponse
 }
