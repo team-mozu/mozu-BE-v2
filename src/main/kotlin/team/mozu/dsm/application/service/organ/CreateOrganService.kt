40467a1ed2048ea31083cc3a592507b8bcd2b5fe
박태수 <taesupark0504@gmail.com>
2025-08-25 23:22:20 +0900

    feat(#52): CreateOrganService

---
+++ src/main/kotlin/team/mozu/dsm/application/service/organ/CreateOrganService.kt
@@ -0,0 +1,28 @@
+package team.mozu.dsm.application.service.organ
+
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import team.mozu.dsm.adapter.`in`.organ.dto.request.CreateOrganCommand
+import team.mozu.dsm.application.port.`in`.organ.CreateOrganUseCase
+import team.mozu.dsm.application.port.out.organ.SaveOrganPort
+import team.mozu.dsm.domain.organ.model.Organ
+
+@Service
+class CreateOrganService(
+    private val saveOrganPort: SaveOrganPort,
+    private val passwordEncoder: PasswordEncoder
+) : CreateOrganUseCase {
+
+    @Transactional
+    override fun create(command: CreateOrganCommand): Organ {
+
+        val organ = Organ(
+            organCode = command.organCode,
+            organName = command.organName,
+            password = passwordEncoder.encode(command.password)
+        )
+
+        return saveOrganPort.save(organ)
+    }
+}
