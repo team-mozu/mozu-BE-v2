88745429eb511ba63e7d81592ab72c848c301538
박지민 <pjmlove2008@gmail.com>
2025-08-25 21:36:01 +0900

    feat(#51): OrderItemMapper

---
+++ src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/mapper/OrderItemMapper.kt
@@ -0,0 +1,30 @@
+package team.mozu.dsm.adapter.out.team.persistence.mapper
+
+import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import team.mozu.dsm.adapter.out.lesson.entity.LessonItemJpaEntity
+import team.mozu.dsm.adapter.out.team.entity.OrderItemJpaEntity
+import team.mozu.dsm.adapter.out.team.entity.TeamJpaEntity
+import team.mozu.dsm.domain.team.model.OrderItem
+
+@Mapper
+abstract class OrderItemMapper {
+
+    @Mapping(target = "teamId", source = "team.id")
+    @Mapping(target = "lessonItemId", source = "lessonItemId")
+    abstract fun toModel(entity: OrderItemJpaEntity): OrderItem
+
+    fun toEntity(model: OrderItem,lessonItem: LessonItemJpaEntity ,team: TeamJpaEntity): OrderItemJpaEntity {
+        return OrderItemJpaEntity(
+            lessonItemId = lessonItem.lessonItemId,
+            lessonItem = lessonItem,
+            team = team,
+            orderType = model.orderType,
+            orderCount = model.orderCount,
+            itemPrice = model.itemPrice,
+            totalAmount = model.totalAmount,
+            orderedAt = model.orderedAt,
+            invCnt = model.invCnt
+        )
+    }
+}
