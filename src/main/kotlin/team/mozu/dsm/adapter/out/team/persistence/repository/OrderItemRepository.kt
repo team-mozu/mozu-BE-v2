8ab0bd6b99e5176364174bf9a9b1366b59655ce4
박지민 <pjmlove2008@gmail.com>
2025-08-25 21:31:34 +0900

    feat(#51): OrderItemRepository

---
+++ src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/repository/OrderItemRepository.kt
@@ -0,0 +1,7 @@
+package team.mozu.dsm.adapter.out.team.persistence.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import team.mozu.dsm.adapter.out.team.entity.OrderItemJpaEntity
+import java.util.UUID
+
+interface OrderItemRepository : JpaRepository<OrderItemJpaEntity, UUID>
