965604f57e1f121a3cc4243e177372a920c4c761
박지민 <pjmlove2008@gmail.com>
2025-08-25 22:19:33 +0900

    feat(#51): OrderItemMapper에 LessonItemMapper 의존성 추가

--- src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/mapper/OrderItemMapper.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/mapper/OrderItemMapper.kt
@@ -3,11 +3,18 @@ package team.mozu.dsm.adapter.out.team.persistence.mapper
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
 import team.mozu.dsm.adapter.out.lesson.entity.LessonItemJpaEntity
+import team.mozu.dsm.adapter.out.lesson.persistence.mapper.LessonItemMapper
 import team.mozu.dsm.adapter.out.team.entity.OrderItemJpaEntity
 import team.mozu.dsm.adapter.out.team.entity.TeamJpaEntity
 import team.mozu.dsm.domain.team.model.OrderItem
 
-@Mapper(componentModel = "spring")
+@Mapper(
+    componentModel = "spring",
+    /**
+     * 중첩된 객체 매핑을 위해 다른 매퍼를 참조
+     */
+    uses = [LessonItemMapper::class]
+)
 abstract class OrderItemMapper {
 
     @Mapping(target = "teamId", source = "team.id")
