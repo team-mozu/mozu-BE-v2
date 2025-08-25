ea36113f83c577dbf5965938e7ed489eacae6173
박태수 <taesupark0504@gmail.com>
2025-08-25 17:04:58 +0900

    refactor(#40): RequestParam으로 변경

--- src/main/kotlin/team/mozu/dsm/adapter/in/sse/SseWebAdapter.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/in/sse/SseWebAdapter.kt
@@ -1,9 +1,8 @@
 package team.mozu.dsm.adapter.`in`.sse
 
-import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 import team.mozu.dsm.application.port.`in`.sse.SubscribeSseUseCase
@@ -16,7 +15,7 @@ class SseWebAdapter(
 
     @GetMapping("/subscribe")
     fun subscribe(
-        @RequestBody @Valid
+        @RequestParam
         clientId: String
     ): SseEmitter {
         return subscribeSseUseCase.subscribe(clientId)
