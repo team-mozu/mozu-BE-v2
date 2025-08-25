a809f280e91a39ed74aff14f763311d47d28df22
박지민 <pjmlove2008@gmail.com>
2025-08-25 14:50:32 +0900

    feat(#48): JwtProperties에 studentAccessExp 추가

--- src/main/kotlin/team/mozu/dsm/global/security/jwt/JwtProperties.kt
+++ src/main/kotlin/team/mozu/dsm/global/security/jwt/JwtProperties.kt
@@ -8,5 +8,10 @@ class JwtProperties(
     val prefix: String,
     val secretKey: String,
     val accessExp: Long,
-    val refreshExp: Long
+    val refreshExp: Long,
+    /**
+     * 학생용 AccessToken 만료 시간 (6시간)
+     * 기관용과는 달리 수업 진행 시간 동안 유지되어야 하므로 별도로 관리
+     */
+    val studentAccessExp: Long
 )
