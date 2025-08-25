37981f5d25185816e0df89ab55e27f23ee28a81e
박지민 <pjmlove2008@gmail.com>
2025-08-26 03:25:39 +0900

    feat(#56): TeamParticipationService

---
+++ src/main/kotlin/team/mozu/dsm/application/team/service/TeamParticipationService.kt
@@ -0,0 +1,52 @@
+package team.mozu.dsm.application.team.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import team.mozu.dsm.adapter.`in`.team.dto.request.TeamParticipationRequest
+import team.mozu.dsm.adapter.`in`.team.dto.response.TeamTokenResponse
+import team.mozu.dsm.application.exception.lesson.LessonIdNotFoundException
+import team.mozu.dsm.application.exception.lesson.LessonNotInProgressException
+import team.mozu.dsm.application.exception.lesson.LessonNumNotFoundException
+import team.mozu.dsm.application.port.out.auth.JwtPort
+import team.mozu.dsm.application.port.out.lesson.LessonPort
+import team.mozu.dsm.application.port.out.team.TeamPort
+import team.mozu.dsm.application.team.port.`in`.TeamParticipationUseCase
+import team.mozu.dsm.domain.team.model.Team
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Service
+class TeamParticipationService(
+    private val lessonPort: LessonPort,
+    private val teamPort: TeamPort,
+    private val jwtPort: JwtPort
+) : TeamParticipationUseCase {
+
+    @Transactional
+    override fun execute(request: TeamParticipationRequest): TeamTokenResponse {
+        val lesson = lessonPort.findByLessonNum(request.lessonNum)
+            ?: throw LessonNumNotFoundException
+
+        if(!lesson.isInProgress) {
+            throw LessonNotInProgressException
+        }
+
+        val team = Team(
+            id = UUID.randomUUID(),
+            lessonId = lesson.id ?: throw LessonIdNotFoundException,
+            teamName = request.teamName,
+            schoolName = request.schoolName,
+            totalMoney = 0L,
+            cashMoney = 0L,
+            valuationMoney = 0L,
+            lessonNum = lesson.lessonNum,
+            isInvestmentInProgress = true, //투자 종료 시 false
+            participationDate = LocalDateTime.now(),
+            createdAt = LocalDateTime.now(),
+            updatedAt = LocalDateTime.now()
+        )
+        teamPort.save(team)
+
+        return jwtPort.createStudentAccessToken(lesson.lessonNum)
+    }
+}
