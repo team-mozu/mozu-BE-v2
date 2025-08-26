eabbc6ce04de6b1242dd48d1a00dc08fa6052b75
박태수 <taesupark0504@gmail.com>
2025-08-26 15:38:53 +0900

    refactor(#58): cors

--- src/main/kotlin/team/mozu/dsm/global/config/security/SecurityConfig.kt
+++ src/main/kotlin/team/mozu/dsm/global/config/security/SecurityConfig.kt
@@ -39,16 +39,10 @@ class SecurityConfig(
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
             .authorizeHttpRequests {
-                //auth
-                it.requestMatchers("/auth/**").permitAll()
-
-                //user
-                it.requestMatchers("/user/**").permitAll()
-                
                 //organ
                 it.requestMatchers(HttpMethod.POST, "/organ/create").permitAll()
                 it.requestMatchers(HttpMethod.POST, "/organ/login").permitAll()
-                it.anyRequest().authenticated()                
+
                 //team
                 it.requestMatchers(HttpMethod.POST, "/team/participate").permitAll()
                     .anyRequest().authenticated()
