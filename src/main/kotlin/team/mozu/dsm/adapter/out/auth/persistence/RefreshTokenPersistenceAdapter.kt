8f4cdf36cacf11851e5489561eb16daa95acef4d
박태수 <taesupark0504@gmail.com>
2025-08-26 20:17:46 +0900

    feat(#63): override deleteToken

--- src/main/kotlin/team/mozu/dsm/adapter/out/auth/persistence/RefreshTokenPersistenceAdapter.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/auth/persistence/RefreshTokenPersistenceAdapter.kt
@@ -3,14 +3,19 @@ package team.mozu.dsm.adapter.out.auth.persistence
 import org.springframework.stereotype.Component
 import team.mozu.dsm.adapter.out.auth.entity.RefreshTokenRedisEntity
 import team.mozu.dsm.adapter.out.auth.persistence.repository.RefreshTokenRepository
+import team.mozu.dsm.application.port.out.auth.CommandTokenPort
 import team.mozu.dsm.application.port.out.auth.QueryTokenPort
 
 @Component
 class RefreshTokenPersistenceAdapter(
     private val refreshTokenRepository: RefreshTokenRepository
-) : QueryTokenPort {
+) : QueryTokenPort, CommandTokenPort {
 
     override fun findByRefreshToken(refreshToken: String): RefreshTokenRedisEntity? {
         return refreshTokenRepository.findByRefreshToken(refreshToken)
+    }
+
+    override fun deleteToken(refreshToken: String): RefreshTokenRedisEntity? {
+        return refreshTokenRepository.deleteToken(refreshToken)
     }
 }
