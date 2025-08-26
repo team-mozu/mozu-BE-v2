f25c2bae75a36da2527070572dcefaa64b961312
박태수 <taesupark0504@gmail.com>
2025-08-26 16:57:45 +0900

    feat(#63): ReissueOrganTokenService

---
+++ src/main/kotlin/team/mozu/dsm/application/service/organ/ReissueOrganTokenService.kt
@@ -0,0 +1,23 @@
+package team.mozu.dsm.application.service.organ
+
+import org.springframework.stereotype.Service
+import team.mozu.dsm.adapter.`in`.auth.dto.response.TokenResponse
+import team.mozu.dsm.adapter.`in`.organ.dto.request.ReissueOrganTokenRequest
+import team.mozu.dsm.application.port.`in`.organ.ReissueOrganTokenUseCase
+import team.mozu.dsm.application.port.out.auth.JwtPort
+import team.mozu.dsm.application.port.out.auth.QueryTokenPort
+import team.mozu.dsm.global.exception.RefreshTokenNotFoundException
+
+@Service
+class ReissueOrganTokenService(
+    private val jwtPort: JwtPort,
+    private val queryTokenPort: QueryTokenPort
+) : ReissueOrganTokenUseCase {
+
+    override fun reissue(request: ReissueOrganTokenRequest): TokenResponse {
+        val refreshToken = queryTokenPort.findByToken(request.token)
+            ?: throw RefreshTokenNotFoundException
+
+        return jwtPort.createToken(refreshToken.organCode)
+    }
+}
