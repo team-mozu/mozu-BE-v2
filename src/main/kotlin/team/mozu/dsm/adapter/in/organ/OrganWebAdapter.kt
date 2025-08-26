0c3068322a2b8f57af9777ed0404a1e53b5b18e1
박태수 <taesupark0504@gmail.com>
2025-08-26 15:02:19 +0900

    feat(#58): webAdapter에 login api추가

--- src/main/kotlin/team/mozu/dsm/adapter/in/organ/OrganWebAdapter.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/in/organ/OrganWebAdapter.kt
@@ -3,14 +3,18 @@ package team.mozu.dsm.adapter.`in`.organ
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import team.mozu.dsm.adapter.`in`.auth.dto.response.TokenResponse
 import team.mozu.dsm.adapter.`in`.organ.dto.request.CreateOrganRequest
+import team.mozu.dsm.adapter.`in`.organ.dto.request.LoginOrganRequest
 import team.mozu.dsm.application.port.`in`.organ.CreateOrganUseCase
+import team.mozu.dsm.application.port.`in`.organ.LoginOrganUseCase
 import team.mozu.dsm.domain.organ.model.Organ
 
 @RestController
 @RequestMapping("/organ")
 class OrganWebAdapter(
-    private val createOrganUseCase: CreateOrganUseCase
+    private val createOrganUseCase: CreateOrganUseCase,
+    private val loginOrganUseCase: LoginOrganUseCase
 ) {
 
     @PostMapping("/create")
@@ -20,5 +24,14 @@ class OrganWebAdapter(
         request: CreateOrganRequest
     ): Organ {
         return createOrganUseCase.create(request)
+    }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    fun login(
+        @RequestBody @Valid
+        request: LoginOrganRequest
+    ): TokenResponse {
+        return loginOrganUseCase.login(request)
     }
 }
