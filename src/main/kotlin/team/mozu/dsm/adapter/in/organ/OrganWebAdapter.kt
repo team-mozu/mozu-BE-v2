734ec4b2b07bee66a98b04902e2e14e1f2e38bf6
박태수 <taesupark0504@gmail.com>
2025-08-26 16:58:03 +0900

    feat(#63): reissue api 추가

--- src/main/kotlin/team/mozu/dsm/adapter/in/organ/OrganWebAdapter.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/in/organ/OrganWebAdapter.kt
@@ -3,14 +3,18 @@ package team.mozu.dsm.adapter.`in`.organ
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import team.mozu.dsm.adapter.`in`.auth.dto.response.TokenResponse
 import team.mozu.dsm.adapter.`in`.organ.dto.request.CreateOrganRequest
+import team.mozu.dsm.adapter.`in`.organ.dto.request.ReissueOrganTokenRequest
 import team.mozu.dsm.application.port.`in`.organ.CreateOrganUseCase
+import team.mozu.dsm.application.port.`in`.organ.ReissueOrganTokenUseCase
 import team.mozu.dsm.domain.organ.model.Organ
 
 @RestController
 @RequestMapping("/organ")
 class OrganWebAdapter(
-    private val createOrganUseCase: CreateOrganUseCase
+    private val createOrganUseCase: CreateOrganUseCase,
+    private val reissueOrganTokenUseCase: ReissueOrganTokenUseCase
 ) {
 
     @PostMapping("/create")
@@ -20,5 +24,14 @@ class OrganWebAdapter(
         request: CreateOrganRequest
     ): Organ {
         return createOrganUseCase.create(request)
+    }
+
+    @PatchMapping("/token/reissue")
+    @ResponseStatus(HttpStatus.OK)
+    fun reissueOrganToken(
+        @RequestBody @Valid
+        request: ReissueOrganTokenRequest
+    ): TokenResponse {
+        return reissueOrganTokenUseCase.reissue(request)
     }
 }
