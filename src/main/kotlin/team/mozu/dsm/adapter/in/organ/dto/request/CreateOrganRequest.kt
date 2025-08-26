a292d5c6e101492b27c5879c17697b26f63b91d4
박태수 <taesupark0504@gmail.com>
2025-08-26 12:31:44 +0900

    refactor(#52): CreateOrganRequest 유효성 검증 어노테이션추가

--- src/main/kotlin/team/mozu/dsm/adapter/in/organ/dto/request/CreateOrganRequest.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/in/organ/dto/request/CreateOrganRequest.kt
@@ -1,7 +1,18 @@
 package team.mozu.dsm.adapter.`in`.organ.dto.request
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
 data class CreateOrganRequest(
+    @field:NotBlank(message = "기관이름을 입력하세요")
+    @field:Size(max = 100, message = "기관이름은 100자 이내로 작성해주세요")
     val organName: String,
+
+    @field:NotBlank(message = "기관코드를 입력하세요")
+    @field:Size(max = 30, message = "기관코드는 30자 이내로 작성해주세요")
     val organCode: String,
+
+    @field:NotBlank(message = "비밀번호를 입력하세요")
+    @field:Size(max = 300, message = "제목은 300자 이내로 작성해주세요")
     val password: String
 )
