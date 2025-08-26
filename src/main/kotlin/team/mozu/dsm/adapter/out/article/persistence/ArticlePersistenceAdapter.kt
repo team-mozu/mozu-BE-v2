2a340cde6cfac7d508fc88756e4e41e59e4f332f
민수아 <Minsua0503@gmail.com>
2025-08-27 03:56:41 +0900

    feat(#53): ArticlePersistenceAdapter

---
+++ src/main/kotlin/team/mozu/dsm/adapter/out/article/persistence/ArticlePersistenceAdapter.kt
@@ -0,0 +1,27 @@
+package team.mozu.dsm.adapter.out.article.persistence
+
+import org.springframework.stereotype.Component
+import team.mozu.dsm.adapter.out.article.persistence.mapper.ArticleMapper
+import team.mozu.dsm.adapter.out.article.persistence.repository.ArticleRepository
+import team.mozu.dsm.application.port.out.article.QueryArticlePort
+import team.mozu.dsm.domain.article.model.Article
+import java.util.*
+
+@Component
+class ArticlePersistenceAdapter(
+    private val articleRepository: ArticleRepository,
+    private val articleMapper: ArticleMapper
+) : QueryArticlePort {
+
+    //--Query--//
+    override fun existsById(id: UUID): Boolean {
+        return articleRepository.existsById(id)
+    }
+
+    override fun findAllByIds(ids: List<UUID>): List<Article> {
+        return articleRepository.findAllById(ids)
+            .map { articleMapper.toModel(it) }
+    }
+
+    //--Command--//
+}
