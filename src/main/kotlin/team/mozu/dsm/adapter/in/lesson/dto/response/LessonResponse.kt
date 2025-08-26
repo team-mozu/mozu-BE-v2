36f4ab0710371a11ab89b26b46e5c7e213bae78c
민수아 <Minsua0503@gmail.com>
2025-08-27 03:37:33 +0900

    feat(#53): LessonResponse

---
+++ src/main/kotlin/team/mozu/dsm/adapter/in/lesson/dto/response/LessonResponse.kt
@@ -0,0 +1,37 @@
+package team.mozu.dsm.adapter.`in`.lesson.dto.response
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import java.time.LocalDateTime
+import java.util.UUID
+
+// 수업 생성, 수정 API에서 공통 사용
+data class LessonResponse(
+    val id: UUID,
+    val name: String,
+    val maxInvRound: Int,
+    val curInvRound: Int? = null,
+    val baseMoney: Long,
+    val lessonNum: String? = null,
+    val isStarred: Boolean,
+    val isDeleted: Boolean,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    val createdAt: LocalDateTime,
+    val lessonArticles: List<LessonArticleResponse>,
+    val lessonItems: List<LessonItemResponse>
+)
+
+data class LessonArticleResponse(
+    val investmentRound: Int,
+    val articles: List<ArticleResponse>
+)
+
+data class ArticleResponse(
+    val articleId: UUID,
+    val title: String
+)
+
+data class LessonItemResponse(
+    val itemId: UUID,
+    val itemName: String,
+    val money: List<Int>
+)
