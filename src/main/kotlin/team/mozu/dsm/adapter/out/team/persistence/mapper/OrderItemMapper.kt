51130b572e5f33bb157fcd76ed3ccb3e00dcfb8d
박지민 <pjmlove2008@gmail.com>
2025-08-26 18:55:11 +0900

    refactor(#62): target을 itemId로 변경

--- src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/mapper/OrderItemMapper.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/mapper/OrderItemMapper.kt
@@ -11,7 +11,7 @@ import team.mozu.dsm.domain.team.model.OrderItem
 abstract class OrderItemMapper {
 
     @Mapping(target = "teamId", source = "team.id")
-    @Mapping(target = "lessonItemId", source = "item.id")
+    @Mapping(target = "itemId", source = "item.id")
     abstract fun toModel(entity: OrderItemJpaEntity): OrderItem
 
     fun toEntity(model: OrderItem, item: ItemJpaEntity, team: TeamJpaEntity): OrderItemJpaEntity {
