2235ff4452314f3111900cd8945e12180ac9b1a7
박태수 <taesupark0504@gmail.com>
2025-08-27 13:34:32 +0900

    feat(#68): queryOrganDetail API organWebAdapter에 추가

--- src/main/kotlin/team/mozu/dsm/adapter/in/organ/OrganWebAdapter.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/in/organ/OrganWebAdapter.kt
@@ -8,16 +8,20 @@ import team.mozu.dsm.adapter.`in`.organ.dto.request.CreateOrganRequest
 import team.mozu.dsm.adapter.`in`.organ.dto.request.ReissueOrganTokenRequest
 import team.mozu.dsm.application.port.`in`.organ.ReissueOrganTokenUseCase
 import team.mozu.dsm.adapter.`in`.organ.dto.request.LoginOrganRequest
+import team.mozu.dsm.adapter.`in`.organ.dto.response.OrganDetailResponse
 import team.mozu.dsm.application.port.`in`.organ.CreateOrganUseCase
 import team.mozu.dsm.application.port.`in`.organ.LoginOrganUseCase
+import team.mozu.dsm.application.port.`in`.organ.QueryOrganDetailUseCase
 import team.mozu.dsm.domain.organ.model.Organ
+import java.util.UUID
 
 @RestController
 @RequestMapping("/organ")
 class OrganWebAdapter(
     private val createOrganUseCase: CreateOrganUseCase,
     private val reissueOrganTokenUseCase: ReissueOrganTokenUseCase,
-    private val loginOrganUseCase: LoginOrganUseCase
+    private val loginOrganUseCase: LoginOrganUseCase,
+    private val queryOrganDetailUseCase: QueryOrganDetailUseCase
 ) {
 
     @PostMapping("/create")
@@ -45,5 +49,11 @@ class OrganWebAdapter(
         request: LoginOrganRequest
     ): TokenResponse {
         return loginOrganUseCase.login(request)
+    }
+
+    @GetMapping("/{organId}")
+    @ResponseStatus(HttpStatus.OK)
+    fun queryOrganDetail(@PathVariable organId: UUID): OrganDetailResponse {
+        return queryOrganDetailUseCase.queryOrganDetail(organId)
     }
 }
