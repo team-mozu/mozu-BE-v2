9754bb69e919da38326077926d5384a0995d25b0
박태수 <taesupark0504@gmail.com>
2025-08-27 13:34:09 +0900

    feat(#68): QueryOrganDetailService

---
+++ src/main/kotlin/team/mozu/dsm/application/service/organ/QueryOrganDetailService.kt
@@ -0,0 +1,18 @@
+package team.mozu.dsm.application.service.organ
+
+import org.springframework.stereotype.Service
+import team.mozu.dsm.adapter.`in`.organ.dto.response.OrganDetailResponse
+import team.mozu.dsm.application.exception.organ.OrganNotFoundException
+import team.mozu.dsm.application.port.`in`.organ.QueryOrganDetailUseCase
+import team.mozu.dsm.application.port.out.organ.QueryOrganPort
+import java.util.*
+
+@Service
+class QueryOrganDetailService(
+    private val queryOrganPort: QueryOrganPort
+) : QueryOrganDetailUseCase {
+
+    override fun queryOrganDetail(organId: UUID): OrganDetailResponse {
+        return queryOrganPort.findById(organId) ?: throw OrganNotFoundException
+    }
+}
