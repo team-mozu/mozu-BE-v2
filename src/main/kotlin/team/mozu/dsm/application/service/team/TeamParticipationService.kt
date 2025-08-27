e13df9a11428ce19d8d6542859518fcfe8739ee1
민수아 <Minsua0503@gmail.com>
2025-08-27 22:00:03 +0900

    fix : import 문제 해결

--- src/main/kotlin/team/mozu/dsm/application/service/team/TeamParticipationService.kt
+++ src/main/kotlin/team/mozu/dsm/application/service/team/TeamParticipationService.kt
@@ -10,6 +10,9 @@ import team.mozu.dsm.adapter.`in`.team.dto.response.TeamTokenResponse
 import team.mozu.dsm.application.exception.lesson.*
 import team.mozu.dsm.application.port.`in`.sse.PublishToAllSseUseCase
 import team.mozu.dsm.application.port.`in`.team.TeamParticipationUseCase
+import team.mozu.dsm.application.port.out.auth.JwtPort
+import team.mozu.dsm.application.port.out.lesson.QueryLessonPort
+import team.mozu.dsm.application.port.out.team.TeamCommandPort
 import team.mozu.dsm.domain.team.model.Team
 import java.time.LocalDateTime
 
@@ -23,7 +26,7 @@ class TeamParticipationService(
 
     @Transactional
     override fun participate(request: TeamParticipationRequest): TeamTokenResponse {
-        val lesson = lessonQueryPort.findByLessonNum(request.lessonNum)
+        val lesson = queryLessonPort.findByLessonNum(request.lessonNum)
             ?: throw LessonNumNotFoundException
 
         if (!lesson.isInProgress) {
