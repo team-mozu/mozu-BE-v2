19b38fe0f0dba4ceaae0ff959b94628fe1de79df
박태수 <taesupark0504@gmail.com>
2025-08-27 13:32:44 +0900

    feat(#68): QueryOrganPort

--- src/main/kotlin/team/mozu/dsm/application/port/out/organ/QueryOrganPort.kt
+++ src/main/kotlin/team/mozu/dsm/application/port/out/organ/QueryOrganPort.kt
@@ -1,8 +1,12 @@
 package team.mozu.dsm.application.port.out.organ
 
+import team.mozu.dsm.adapter.`in`.organ.dto.response.OrganDetailResponse
 import team.mozu.dsm.domain.organ.model.Organ
+import java.util.UUID
 
 interface QueryOrganPort {
 
     fun findByOrganCode(organCode: String): Organ?
+
+    fun findById(id: UUID): OrganDetailResponse?
 }
