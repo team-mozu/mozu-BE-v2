4460c699f1e3596bac0fcec70e17eb0a3069e2fa
박태수 <taesupark0504@gmail.com>
2025-08-27 14:07:17 +0900

    refactor(#68): 단일 기관 조회 시 organFacade적용

--- src/main/kotlin/team/mozu/dsm/application/service/organ/QueryOrganDetailService.kt
+++ src/main/kotlin/team/mozu/dsm/application/service/organ/QueryOrganDetailService.kt
@@ -5,14 +5,18 @@ import team.mozu.dsm.adapter.`in`.organ.dto.response.OrganDetailResponse
 import team.mozu.dsm.application.exception.organ.OrganNotFoundException
 import team.mozu.dsm.application.port.`in`.organ.QueryOrganDetailUseCase
 import team.mozu.dsm.application.port.out.organ.QueryOrganPort
+import team.mozu.dsm.application.service.organ.facade.OrganFacade
 import java.util.*
 
 @Service
 class QueryOrganDetailService(
-    private val queryOrganPort: QueryOrganPort
+    private val queryOrganPort: QueryOrganPort,
+    private val organFacade: OrganFacade
 ) : QueryOrganDetailUseCase {
 
     override fun queryOrganDetail(id: UUID): OrganDetailResponse {
+        organFacade.currentOrgan()
+
         return queryOrganPort.findById(id) ?: throw OrganNotFoundException
     }
 }
