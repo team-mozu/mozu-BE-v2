a9c89ff420412deb4fa987427bd340dfc6f60c15
박지민 <pjmlove2008@gmail.com>
2025-08-28 12:42:42 +0900

    feat(#62): ItemPersistenceAdapter

---
+++ src/main/kotlin/team/mozu/dsm/adapter/out/item/persistence/ItemPersistenceAdapter.kt
@@ -0,0 +1,23 @@
+package team.mozu.dsm.adapter.out.item.persistence
+
+import org.springframework.stereotype.Component
+import team.mozu.dsm.adapter.out.item.persistence.mapper.ItemMapper
+import team.mozu.dsm.adapter.out.item.persistence.repository.ItemRepository
+import team.mozu.dsm.application.exception.item.ItemNotFoundException
+import team.mozu.dsm.application.port.out.item.ItemQueryPort
+import team.mozu.dsm.domain.item.model.Item
+import java.util.*
+
+@Component
+class ItemPersistenceAdapter(
+    private val itemRepository: ItemRepository,
+    private val itemMapper: ItemMapper
+) : ItemQueryPort {
+
+    override fun findById(itemId: UUID): Item? {
+        val itemEntity = itemRepository.findById(itemId)
+            .orElseThrow { ItemNotFoundException }
+
+        return itemMapper.toModel(itemEntity)
+    }
+}
