4055735ad34219713e107a3853e78a59c0dd463c
박지민 <pjmlove2008@gmail.com>
2025-08-28 13:11:14 +0900

    feat(#62): LessonItemRepository

--- src/main/kotlin/team/mozu/dsm/adapter/out/lesson/persistence/repository/LessonItemRepository.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/lesson/persistence/repository/LessonItemRepository.kt
@@ -1,7 +1,16 @@
 package team.mozu.dsm.adapter.out.lesson.persistence.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import team.mozu.dsm.adapter.out.lesson.entity.LessonItemJpaEntity
 import team.mozu.dsm.adapter.out.lesson.entity.id.LessonItemId
+import java.util.UUID
 
-interface LessonItemRepository : JpaRepository<LessonItemJpaEntity, LessonItemId>
+interface LessonItemRepository : JpaRepository<LessonItemJpaEntity, LessonItemId> {
+
+    @Query("SELECT li.lessonItemId.itemId FROM LessonItemJpaEntity li WHERE li.lessonItemId.lessonId = :lessonId")
+    fun findItemIdsByLessonId(@Param("lessonId") lessonId: UUID): List<UUID>
+
+    fun findAllByLessonIdAndItemIdIn(lessonId: UUID, itemIds: List<UUID>): List<LessonItemJpaEntity>
+}
