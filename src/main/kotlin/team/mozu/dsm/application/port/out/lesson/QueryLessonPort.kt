cdc99b887648664213734d4e57957d15edaedc58
민수아 <Minsua0503@gmail.com>
2025-08-28 17:01:54 +0900

    feat(#71): findById, existsByLessonNum 쿼리 함수 추가

--- src/main/kotlin/team/mozu/dsm/application/port/out/lesson/QueryLessonPort.kt
+++ src/main/kotlin/team/mozu/dsm/application/port/out/lesson/QueryLessonPort.kt
@@ -1,8 +1,13 @@
 package team.mozu.dsm.application.port.out.lesson
 
 import team.mozu.dsm.domain.lesson.model.Lesson
+import java.util.UUID
 
 interface QueryLessonPort {
 
     fun findByLessonNum(lessonNum: String): Lesson?
+
+    fun findById(id: UUID): Lesson
+
+    fun existsByLessonNum(lessonNum: String): Boolean
 }
