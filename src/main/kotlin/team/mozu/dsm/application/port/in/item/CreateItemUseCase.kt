23081e47f3c4680adb77078f00f00a77275c8755
imhanseong <hans_hanseong@dsm.hs.kr>
2025-08-28 20:37:09 +0900

    feat(#72): CreateItemUseCase 생성

---
+++ src/main/kotlin/team/mozu/dsm/application/port/in/item/CreateItemUseCase.kt
@@ -0,0 +1,8 @@
+package team.mozu.dsm.application.port.`in`.item
+
+import team.mozu.dsm.adapter.`in`.item.dto.request.ItemRequest
+import team.mozu.dsm.adapter.`in`.item.dto.response.ItemResponse
+
+interface CreateItemUseCase {
+    fun create(request: ItemRequest) : ItemResponse
+}
