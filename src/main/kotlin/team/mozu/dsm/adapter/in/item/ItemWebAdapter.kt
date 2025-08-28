63a5c3072abdb5d83042cb8da46f4108cd0ebde6
imhanseong <hans_hanseong@dsm.hs.kr>
2025-08-28 20:35:02 +0900

    feat(#72): ItemWebAdapter 생성

---
+++ src/main/kotlin/team/mozu/dsm/adapter/in/item/ItemWebAdapter.kt
@@ -0,0 +1,28 @@
+package team.mozu.dsm.adapter.`in`.item
+
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import team.mozu.dsm.adapter.`in`.item.dto.request.ItemRequest
+import team.mozu.dsm.adapter.`in`.item.dto.response.ItemResponse
+import team.mozu.dsm.application.port.`in`.item.CreateItemUseCase
+
+@RestController
+@RequestMapping("item")
+class ItemWebAdapter (
+    private val createItemUseCase: CreateItemUseCase
+){
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create (
+        @RequestBody @Valid
+        request: ItemRequest
+    ) : ItemResponse {
+        return createItemUseCase.create(request)
+    }
+}
