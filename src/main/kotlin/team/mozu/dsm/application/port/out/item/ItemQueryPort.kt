d3e7c5caeb7995fe70d74c146fafa19b59d14f67
박지민 <pjmlove2008@gmail.com>
2025-08-28 12:43:03 +0900

    feat(#62): ItemQueryPort

---
+++ src/main/kotlin/team/mozu/dsm/application/port/out/item/ItemQueryPort.kt
@@ -0,0 +1,9 @@
+package team.mozu.dsm.application.port.out.item
+
+import team.mozu.dsm.domain.item.model.Item
+import java.util.UUID
+
+interface ItemQueryPort {
+
+    fun findById(itemId: UUID): Item?
+}
