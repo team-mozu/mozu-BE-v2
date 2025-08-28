8ca500cbd9348b23811d7d32c6badae0bea3a45a
박지민 <pjmlove2008@gmail.com>
2025-08-28 13:02:53 +0900

    feat(#62): TeamPersistenceAdapter

--- src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/TeamPersistenceAdapter.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/TeamPersistenceAdapter.kt
@@ -5,22 +5,48 @@ import team.mozu.dsm.adapter.out.lesson.persistence.repository.LessonRepository
 import team.mozu.dsm.adapter.out.team.persistence.mapper.TeamMapper
 import team.mozu.dsm.adapter.out.team.persistence.repository.TeamRepository
 import team.mozu.dsm.application.exception.lesson.LessonNotFoundException
+import team.mozu.dsm.application.exception.team.TeamNotFoundException
 import team.mozu.dsm.application.port.out.team.TeamCommandPort
+import team.mozu.dsm.application.port.out.team.TeamQueryPort
 import team.mozu.dsm.domain.team.model.Team
+import java.util.*
 
 @Component
 class TeamPersistenceAdapter(
     private val teamRepository: TeamRepository,
     private val lessonRepository: LessonRepository,
     private val teamMapper: TeamMapper
-) : TeamCommandPort {
+) : TeamCommandPort, TeamQueryPort {
+
+    //--Query--//
+    override fun findById(teamId: UUID): Team {
+        val entity = teamRepository.findById(teamId)
+            .orElseThrow { TeamNotFoundException }
+
+        return teamMapper.toModel(entity)
+    }
+
+    //--Command--//
     override fun save(team: Team): Team {
         val lessonEntity = lessonRepository.findById(team.lessonId)
             .orElseThrow { LessonNotFoundException }
-        val entity = teamMapper.toEntity(team, lessonEntity)
 
-        val saved = teamRepository.save(entity)
+        val entity = if (team.id != null) {
+            val existingEntity = teamRepository.findById(team.id)
+                .orElseThrow { TeamNotFoundException }
 
-        return teamMapper.toModel(saved)
+            existingEntity.apply {
+                cashMoney = team.cashMoney
+                valuationMoney = team.valuationMoney
+                totalMoney = team.totalMoney
+                isInvestmentInProgress = team.isInvestmentInProgress
+                updatedAt = team.updatedAt
+            }
+        } else {
+            teamMapper.toEntity(team, lessonEntity)
+        }
+
+        val savedEntity = teamRepository.save(entity)
+        return teamMapper.toModel(savedEntity)
     }
 }
