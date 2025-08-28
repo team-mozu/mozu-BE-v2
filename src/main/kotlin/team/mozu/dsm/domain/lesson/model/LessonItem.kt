9e4fd997b1e60cd7ce89e624a3cf4a02beb3dec3
박지민 <pjmlove2008@gmail.com>
2025-08-28 12:00:00 +0900

    feat(#62): LessonItem에 특정 차수 가격 조회 메서드 추가

--- src/main/kotlin/team/mozu/dsm/domain/lesson/model/LessonItem.kt
+++ src/main/kotlin/team/mozu/dsm/domain/lesson/model/LessonItem.kt
@@ -14,4 +14,21 @@ data class LessonItem(
     val round6Money: Int?,
     val round7Money: Int?,
     val round8Money: Int?
-)
+) {
+    /**
+     * 특정 차수의 가격 조회
+     */
+    fun getPriceByRound(round: Int): Int? {
+        return when (round) {
+            1 -> round1Money
+            2 -> round2Money
+            3 -> round3Money
+            4 -> round4Money
+            5 -> round5Money
+            6 -> round6Money
+            7 -> round7Money
+            8 -> round8Money
+            else -> null
+        }
+    }
+}
