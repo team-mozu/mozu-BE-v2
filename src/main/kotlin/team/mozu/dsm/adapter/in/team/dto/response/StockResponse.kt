af06a5063b76ec3f84f8f285b0a61f460d037123
박지민 <pjmlove2008@gmail.com>
2025-09-01 14:42:15 +0900

    feat(#85): StockResponse

---
+++ src/main/kotlin/team/mozu/dsm/adapter/in/team/dto/response/StockResponse.kt
@@ -0,0 +1,16 @@
+package team.mozu.dsm.adapter.`in`.team.dto.response
+
+import java.util.UUID
+
+data class StockResponse(
+    val id: UUID,
+    val itemId: UUID,
+    val itemName: String,
+    val avgPurchasePrice: Long,
+    val quantity: Int,
+    val totalMoney: Long,
+    val nowMoney: Long,
+    val valuationMoney: Long,
+    val valProfit: Long,
+    val profitNum: Double
+)
