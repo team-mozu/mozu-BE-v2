92bd3245533a88fc7c77e473accd3fd2d31cdccd
박태수 <taesupark0504@gmail.com>
2025-09-01 19:22:47 +0900

    feat(#75): findOrganInventory()

--- src/main/kotlin/team/mozu/dsm/application/port/out/organ/QueryOrganPort.kt
+++ src/main/kotlin/team/mozu/dsm/application/port/out/organ/QueryOrganPort.kt
@@ -1,6 +1,7 @@
 package team.mozu.dsm.application.port.out.organ
 
 import team.mozu.dsm.adapter.`in`.organ.dto.response.OrganDetailResponse
+import team.mozu.dsm.adapter.`in`.organ.dto.response.OrganListResponse
 import team.mozu.dsm.domain.organ.model.Organ
 import java.util.UUID
 
@@ -9,4 +10,6 @@ interface QueryOrganPort {
     fun findByOrganCode(organCode: String): Organ?
 
     fun findById(id: UUID): OrganDetailResponse?
+
+    fun findOrganInventory(): List<OrganListResponse>
 }
