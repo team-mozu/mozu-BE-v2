6939556a99656f96d51fff800d5abb5ba69991a6
박태수 <taesupark0504@gmail.com>
2025-09-01 19:21:19 +0900

    feat(#75): q클래스 등록

--- src/main/kotlin/team/mozu/dsm/adapter/in/organ/dto/response/OrganListResponse.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/in/organ/dto/response/OrganListResponse.kt
@@ -1,8 +1,9 @@
 package team.mozu.dsm.adapter.`in`.organ.dto.response
 
+import com.querydsl.core.annotations.QueryProjection
 import java.util.UUID
 
-data class OrganListResponse(
+data class OrganListResponse @QueryProjection constructor(
     val id: UUID,
     val organCode: String,
     val organName: String,
