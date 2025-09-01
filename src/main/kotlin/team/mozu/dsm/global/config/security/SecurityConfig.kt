e42fb559704940774f62f4d3fa8ac2514cdbf09c
박태수 <taesupark0504@gmail.com>
2025-09-01 19:23:58 +0900

    feat(#75): get /organ 경로 허용

--- src/main/kotlin/team/mozu/dsm/global/config/security/SecurityConfig.kt
+++ src/main/kotlin/team/mozu/dsm/global/config/security/SecurityConfig.kt
@@ -44,6 +44,7 @@ class SecurityConfig(
                 it.requestMatchers(HttpMethod.PATCH, "/organ/token/reissue").permitAll()
                 it.requestMatchers(HttpMethod.POST, "/organ/login").permitAll()
                 it.requestMatchers(HttpMethod.GET, "/organ/{id}").permitAll()
+                it.requestMatchers(HttpMethod.GET, "/organ").permitAll()
 
                 //team
                 it.requestMatchers(HttpMethod.POST, "/team/participate").permitAll()
