f5fffdc5b3a21c8a4e995ad7861a9c81d1e0ba44
박태수 <taesupark0504@gmail.com>
2025-09-01 22:26:29 +0900

    refactor(#75): facade제거

--- src/main/kotlin/team/mozu/dsm/application/service/organ/QueryOrganInventoryService.kt
+++ src/main/kotlin/team/mozu/dsm/application/service/organ/QueryOrganInventoryService.kt
@@ -4,16 +4,13 @@ import org.springframework.stereotype.Service
 import team.mozu.dsm.adapter.`in`.organ.dto.response.OrganListResponse
 import team.mozu.dsm.application.port.`in`.organ.QueryOrganInventoryUseCase
 import team.mozu.dsm.application.port.out.organ.QueryOrganPort
-import team.mozu.dsm.application.service.organ.facade.OrganFacade
 
 @Service
 class QueryOrganInventoryService(
-    private val queryOrganPort: QueryOrganPort,
-    private val organFacade: OrganFacade
+    private val queryOrganPort: QueryOrganPort
 ) : QueryOrganInventoryUseCase {
 
     override fun queryOrganInventory(): List<OrganListResponse> {
-        organFacade.currentOrgan()
 
         return queryOrganPort.findOrganInventory()
     }
