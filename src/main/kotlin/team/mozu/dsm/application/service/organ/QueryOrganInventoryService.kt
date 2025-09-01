d006a3b16bd943adf472b7621b30b203899813bc
박태수 <taesupark0504@gmail.com>
2025-09-01 19:23:25 +0900

    feat(#75):queryOrganInventoryService

---
+++ src/main/kotlin/team/mozu/dsm/application/service/organ/QueryOrganInventoryService.kt
@@ -0,0 +1,20 @@
+package team.mozu.dsm.application.service.organ
+
+import org.springframework.stereotype.Service
+import team.mozu.dsm.adapter.`in`.organ.dto.response.OrganListResponse
+import team.mozu.dsm.application.port.`in`.organ.QueryOrganInventoryUseCase
+import team.mozu.dsm.application.port.out.organ.QueryOrganPort
+import team.mozu.dsm.application.service.organ.facade.OrganFacade
+
+@Service
+class QueryOrganInventoryService(
+    private val queryOrganPort: QueryOrganPort,
+    private val organFacade: OrganFacade
+): QueryOrganInventoryUseCase {
+
+    override fun queryOrganInventory(): List<OrganListResponse> {
+        organFacade.currentOrgan()
+
+        return queryOrganPort.findOrganInventory()
+    }
+}
