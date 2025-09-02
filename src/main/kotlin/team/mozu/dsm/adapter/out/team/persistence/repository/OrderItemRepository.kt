e160ec1491c373653b7688300d1464da04ef01c0
박지민 <pjmlove2008@gmail.com>
2025-09-02 17:15:46 +0900

    feat(#103): OrderItemRepository

--- src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/repository/OrderItemRepository.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/repository/OrderItemRepository.kt
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import team.mozu.dsm.adapter.out.team.entity.OrderItemJpaEntity
 import java.util.UUID
 
-interface OrderItemRepository : JpaRepository<OrderItemJpaEntity, UUID>
+interface OrderItemRepository : JpaRepository<OrderItemJpaEntity, UUID> {
+
+    fun findAllByTeamId(teamId: UUID): List<OrderItemJpaEntity>
+}
