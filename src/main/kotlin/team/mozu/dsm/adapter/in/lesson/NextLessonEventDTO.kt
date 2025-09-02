e17d2ce6db8865878e8104836735f9832189c011
민수아 <Minsua0503@gmail.com>
2025-09-02 22:37:54 +0900

    feat(#108): NextLessonEventDTO

---
+++ src/main/kotlin/team/mozu/dsm/adapter/in/lesson/NextLessonEventDTO.kt
@@ -0,0 +1,11 @@
+package team.mozu.dsm.adapter.`in`.lesson
+
+import java.util.UUID
+
+data class NextLessonEventDTO(
+    val lessonId: UUID,
+    val curInvRound: Int,
+    val teamId: UUID,
+    val teamName: String?,
+    val schoolName: String
+)
