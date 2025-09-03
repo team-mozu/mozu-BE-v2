bb7917e99e691a74b8162f807778d9717584be49
박지민 <pjmlove2008@gmail.com>
2025-09-03 21:54:31 +0900

    feat(#118): GetTeamRanksService

---
+++ src/main/kotlin/team/mozu/dsm/application/service/team/GetTeamRanksService.kt
@@ -0,0 +1,29 @@
+package team.mozu.dsm.application.service.team
+
+import org.springframework.stereotype.Service
+import team.mozu.dsm.adapter.`in`.team.dto.response.TeamRankResponse
+import team.mozu.dsm.application.port.`in`.team.GetTeamRanksUseCase
+import team.mozu.dsm.application.port.out.team.QueryTeamPort
+import java.util.UUID
+
+@Service
+class GetTeamRanksService(
+    private val queryTeamPort: QueryTeamPort
+) : GetTeamRanksUseCase {
+
+    override fun get(lessonNum: String, teamId: UUID): List<TeamRankResponse> {
+
+        val allTeams = queryTeamPort.findAllByLessonNumOrderByTotalMoneyDesc(lessonNum)
+
+        return allTeams
+            .map { team ->
+                TeamRankResponse(
+                    id = team.id!!,
+                    teamName = team.teamName,
+                    schoolName = team.schoolName,
+                    totalMoney = team.totalMoney,
+                    isMyTeam = team.id == teamId
+                )
+            }
+    }
+}
