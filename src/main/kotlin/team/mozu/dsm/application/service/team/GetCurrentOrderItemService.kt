1c6f85801fb56295ac051f04fe4941ba6d291116
박태수 <taesupark0504@gmail.com>
2025-09-03 22:59:44 +0900

    feat(#117): getCurrentOrderItemService

---
+++ src/main/kotlin/team/mozu/dsm/application/service/team/GetCurrentOrderItemService.kt
@@ -0,0 +1,20 @@
+package team.mozu.dsm.application.service.team
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import team.mozu.dsm.adapter.`in`.team.dto.response.OrderItemResponse
+import team.mozu.dsm.application.exception.item.ItemNotFoundException
+import team.mozu.dsm.application.port.`in`.team.GetCurrentOrderItemUseCase
+import team.mozu.dsm.application.port.out.team.QueryOrderItemPort
+import java.util.*
+
+@Service
+class GetCurrentOrderItemService(
+    private val queryOrderItemPort: QueryOrderItemPort
+) : GetCurrentOrderItemUseCase {
+
+    @Transactional(readOnly = true)
+    override fun getCurrentOrderItem(teamId: UUID): OrderItemResponse {
+        return queryOrderItemPort.findByTeamId(teamId) ?: throw ItemNotFoundException
+    }
+}
