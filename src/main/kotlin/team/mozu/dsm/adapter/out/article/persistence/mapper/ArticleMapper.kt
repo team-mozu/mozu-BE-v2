93d77b3854a7e6bfbe2501028d634edabbf17b60
imhanseong <hans_hanseong@dsm.hs.kr>
2025-09-03 23:03:40 +0900

    refactor(#120): Article 관련 추가

--- src/main/kotlin/team/mozu/dsm/adapter/out/article/persistence/mapper/ArticleMapper.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/article/persistence/mapper/ArticleMapper.kt
@@ -2,6 +2,7 @@ package team.mozu.dsm.adapter.out.article.persistence.mapper
 
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import team.mozu.dsm.adapter.`in`.article.dto.response.ArticleResponse
 import team.mozu.dsm.adapter.out.article.entity.ArticleJpaEntity
 import team.mozu.dsm.adapter.out.organ.entity.OrganJpaEntity
 import team.mozu.dsm.domain.article.model.Article
@@ -11,6 +12,10 @@ abstract class ArticleMapper {
 
     @Mapping(target = "organId", source = "organ.id")
     abstract fun toModel(entity: ArticleJpaEntity): Article
+    @Mapping(target = "articleDesc", source = "articleDescription")
+    @Mapping(target = "articleImg",  source = "articleImage", defaultValue = "")
+
+    abstract fun toResponse(model: Article): ArticleResponse
 
     fun toEntity(model: Article, organ: OrganJpaEntity): ArticleJpaEntity {
         return ArticleJpaEntity(
