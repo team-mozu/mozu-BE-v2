bbe3ff4ac255ab798c67d6850bed7ac15579cb6b
박태수 <taesupark0504@gmail.com>
2025-09-03 22:58:55 +0900

    feat(#117): findByTeamId port에 추가

--- src/main/kotlin/team/mozu/dsm/application/port/out/team/QueryOrderItemPort.kt
+++ src/main/kotlin/team/mozu/dsm/application/port/out/team/QueryOrderItemPort.kt
@@ -1,9 +1,12 @@
 package team.mozu.dsm.application.port.out.team
 
+import team.mozu.dsm.adapter.`in`.team.dto.response.OrderItemResponse
 import team.mozu.dsm.domain.team.model.OrderItem
 import java.util.UUID
 
 interface QueryOrderItemPort {
 
     fun findAllByTeamId(teamId: UUID): List<OrderItem>
+
+    fun findByTeamId(teamId: UUID): OrderItemResponse?
 }
