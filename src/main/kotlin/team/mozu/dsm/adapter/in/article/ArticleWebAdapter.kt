b20c740c294abd04561438b8a27ad0dc7dde8e33
imhanseong <hans_hanseong@dsm.hs.kr>
2025-09-03 23:00:30 +0900

    feat(#120): ArticleWebAdapter 추가

---
+++ src/main/kotlin/team/mozu/dsm/adapter/in/article/ArticleWebAdapter.kt
@@ -0,0 +1,28 @@
+package team.mozu.dsm.adapter.`in`.article
+
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import team.mozu.dsm.adapter.`in`.article.dto.request.ArticleRequest
+import team.mozu.dsm.adapter.`in`.article.dto.response.ArticleResponse
+import team.mozu.dsm.application.port.`in`.article.CreateArticleUseCase
+
+@RestController
+@RequestMapping("article")
+class ArticleWebAdapter(
+    private val createArticleUseCase: CreateArticleUseCase
+) {
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(
+        @RequestBody @Valid
+        request: ArticleRequest
+    ): ArticleResponse {
+        return createArticleUseCase.create(request)
+    }
+}
