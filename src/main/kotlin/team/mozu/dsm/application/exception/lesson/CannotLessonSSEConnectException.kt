f128dc904224c6192ee84381e20ae87edcde249c
민수아 <Minsua0503@gmail.com>
2025-09-04 23:22:00 +0900

    feat(#145): CannotLessonSSEConnectException

---
+++ src/main/kotlin/team/mozu/dsm/application/exception/lesson/CannotLessonSSEConnectException.kt
@@ -0,0 +1,6 @@
+package team.mozu.dsm.application.exception.lesson
+
+import team.mozu.dsm.global.error.exception.ErrorCode
+import team.mozu.dsm.global.error.exception.MozuException
+
+object CannotLessonSSEConnectException: MozuException(ErrorCode.CANNOT_LESSON_SSE_CONNECT)
