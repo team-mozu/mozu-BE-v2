68eb2a1fd9b8c097aab8bdc21f31a785993a6f68
박태수 <taesupark0504@gmail.com>
2025-09-04 22:25:17 +0900

    chore(#143): 이상한거 고치기

--- src/main/kotlin/team/mozu/dsm/application/port/out/item/QueryItemPort.kt
+++ src/main/kotlin/team/mozu/dsm/application/port/out/item/QueryItemPort.kt
@@ -7,7 +7,7 @@ interface QueryItemPort {
 
     fun existsById(id: UUID): Boolean
 
-    fun findAllByIds(ids: Set<UUID>): List<Item>
+    fun findAllByIds(ids: List<UUID>): List<Item>
 
     fun findById(id: UUID): Item?
 
