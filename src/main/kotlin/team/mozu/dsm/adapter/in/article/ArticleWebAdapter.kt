2f288dd0d54b0220ec6d054dc9f010c52c3b65cc
imhanseong <hans_hanseong@dsm.hs.kr>
2025-09-04 18:28:09 +0900

    refactor(#135): 조회 관련 추가

--- src/main/kotlin/team/mozu/dsm/adapter/in/article/ArticleWebAdapter.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/in/article/ArticleWebAdapter.kt
@@ -2,19 +2,23 @@ package team.mozu.dsm.adapter.`in`.article
 
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import team.mozu.dsm.adapter.`in`.article.dto.request.ArticleRequest
 import team.mozu.dsm.adapter.`in`.article.dto.response.ArticleResponse
+import team.mozu.dsm.adapter.`in`.item.dto.response.ItemResponse
+import team.mozu.dsm.adapter.out.article.persistence.mapper.ArticleMapper
 import team.mozu.dsm.application.port.`in`.article.CreateArticleUseCase
+import team.mozu.dsm.application.port.`in`.article.QueryArticleAllUseCase
+import team.mozu.dsm.application.port.`in`.article.QueryArticleDetailUseCase
+import java.util.*
 
 @RestController
 @RequestMapping("/article")
 class ArticleWebAdapter(
-    private val createArticleUseCase: CreateArticleUseCase
+    private val createArticleUseCase: CreateArticleUseCase,
+    private val queryArticleDetailUseCase: QueryArticleDetailUseCase,
+    private val queryArticleAllUseCase: QueryArticleAllUseCase,
+    private val articleMapper: ArticleMapper
 ) {
 
     @PostMapping
@@ -24,5 +28,20 @@ class ArticleWebAdapter(
         request: ArticleRequest
     ): ArticleResponse {
         return createArticleUseCase.create(request)
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun queryDetail(
+        @PathVariable id: UUID
+    ): ArticleResponse {
+        val article = queryArticleDetailUseCase.queryDetail(id)
+        return articleMapper.toResponse(article)
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    fun queryAll(): List<ArticleResponse> {
+        return queryArticleAllUseCase.queryAll()
     }
 }
