32605438a09c57acc3d67507c88e367b7d275b24
imhanseong <hans_hanseong@dsm.hs.kr>
2025-09-04 18:28:33 +0900

    refactor(#135): 조회 관련 추가

--- src/main/kotlin/team/mozu/dsm/application/port/out/article/QueryArticlePort.kt
+++ src/main/kotlin/team/mozu/dsm/application/port/out/article/QueryArticlePort.kt
@@ -1,6 +1,7 @@
 package team.mozu.dsm.application.port.out.article
 
 import team.mozu.dsm.domain.article.model.Article
+import team.mozu.dsm.domain.item.model.Item
 import java.util.UUID
 
 interface QueryArticlePort {
@@ -8,4 +9,8 @@ interface QueryArticlePort {
     fun existsById(id: UUID): Boolean
 
     fun findAllByIds(ids: Set<UUID>): List<Article>
+
+    fun findById(id: UUID): Article?
+
+    fun findAll(): List<Article>
 }
