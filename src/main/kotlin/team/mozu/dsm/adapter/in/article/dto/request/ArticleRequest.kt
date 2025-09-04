973dcccb310f8b1b398e142f429f6a2256903b95
imhanseong <hans_hanseong@dsm.hs.kr>
2025-09-04 16:45:06 +0900

    refactor(#120): articleImage 추가

--- src/main/kotlin/team/mozu/dsm/adapter/in/article/dto/request/ArticleRequest.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/in/article/dto/request/ArticleRequest.kt
@@ -10,5 +10,8 @@ data class ArticleRequest(
 
     @field:NotBlank(message = "기사 본문 내용은 필수 입력입니다.")
     @field:Size(min = 1, max = 10_000, message = "기사 본문은 1~10000자 이내로 입력해주세요.")
-    val articleDesc: String
+    val articleDesc: String,
+
+    @field:Size(max = 2000, message = "이미지 URL은 최대 2000자까지 입력 가능합니다.")
+    val articleImage: String? = null
 )
