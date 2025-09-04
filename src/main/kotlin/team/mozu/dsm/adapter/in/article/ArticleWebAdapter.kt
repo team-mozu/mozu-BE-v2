e3bbf3ceea3e6a0f4d699df4423d5dd227de41e9
imhanseong <hans_hanseong@dsm.hs.kr>
2025-09-04 19:39:19 +0900

    refactor(#137): 삭제 관련 추가

--- src/main/kotlin/team/mozu/dsm/adapter/in/article/ArticleWebAdapter.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/in/article/ArticleWebAdapter.kt
@@ -8,6 +8,7 @@ import team.mozu.dsm.adapter.`in`.article.dto.response.ArticleResponse
 import team.mozu.dsm.adapter.`in`.item.dto.response.ItemResponse
 import team.mozu.dsm.adapter.out.article.persistence.mapper.ArticleMapper
 import team.mozu.dsm.application.port.`in`.article.CreateArticleUseCase
+import team.mozu.dsm.application.port.`in`.article.DeleteArticleUseCase
 import team.mozu.dsm.application.port.`in`.article.QueryArticleAllUseCase
 import team.mozu.dsm.application.port.`in`.article.QueryArticleDetailUseCase
 import java.util.*
@@ -18,6 +19,7 @@ class ArticleWebAdapter(
     private val createArticleUseCase: CreateArticleUseCase,
     private val queryArticleDetailUseCase: QueryArticleDetailUseCase,
     private val queryArticleAllUseCase: QueryArticleAllUseCase,
+    private val deleteArticleUseCase: DeleteArticleUseCase,
     private val articleMapper: ArticleMapper
 ) {
 
@@ -43,5 +45,13 @@ class ArticleWebAdapter(
     @ResponseStatus(HttpStatus.OK)
     fun queryAll(): List<ArticleResponse> {
         return queryArticleAllUseCase.queryAll()
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(
+        @PathVariable id: UUID
+    ){
+        deleteArticleUseCase.delete(id)
     }
 }
