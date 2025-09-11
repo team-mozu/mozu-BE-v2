24d15b3c3e7e23173a0d28a66186be2e8e494ef2
박태수 <taesupark0504@gmail.com>
2025-09-11 13:53:09 +0900

    chore: 중복 클래스 제거

--- src/main/kotlin/team/mozu/dsm/global/config/ConfigurationPropertiesConfig.kt
+++
@@ -1,8 +0,0 @@
-package team.mozu.dsm.global.config
-
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@ConfigurationPropertiesScan(basePackages = ["team.mozu.dsm"])
-class ConfigurationPropertiesConfig
