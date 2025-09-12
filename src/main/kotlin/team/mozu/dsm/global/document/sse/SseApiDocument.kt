ba8db627d5fae51c578c94bdd536d32edd4ee6e8
coehgns <modooboiroo@gmail.com>
2025-09-12 21:45:21 +0900

    feat(#149): SseApiDocument 추가

---
+++ src/main/kotlin/team/mozu/dsm/global/document/sse/SseApiDocument.kt
@@ -0,0 +1,43 @@
+package team.mozu.dsm.global.document.sse
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+
+@Tag(name = "SSE", description = "Server-Sent Events 관련 API")
+interface SseApiDocument {
+
+    @Operation(
+        summary = "SSE 구독",
+        description = "클라이언트 ID를 사용하여 Server-Sent Events(SSE)를 구독합니다."
+    )
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "SSE 연결 성공"
+        ),
+        ApiResponse(
+            responseCode = "400",
+            description = "잘못된 클라이언트 ID"
+        ),
+        ApiResponse(
+            responseCode = "500",
+            description = "서버 내부 오류"
+        )
+    )
+    @GetMapping(
+        value = ["/subscribe"],
+        produces = [MediaType.TEXT_EVENT_STREAM_VALUE]
+    )
+    fun subscribe(
+        @Parameter(description = "클라이언트 고유 식별자", required = true, example = "client-1234")
+        @RequestParam
+        clientId: String
+    ): SseEmitter
+}
