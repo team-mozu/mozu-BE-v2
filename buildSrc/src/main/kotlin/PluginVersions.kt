6dd2887a988216f6506bd5580863180aa546ea29
coehgns <modooboiroo@gmail.com>
2025-09-13 23:27:38 +0900

    chore(#149): Spring, Spring management plugin version 수정

--- buildSrc/src/main/kotlin/PluginVersions.kt
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -1,7 +1,7 @@
 object PluginVersions {
     const val KOTLIN_VERSION = "1.9.25"
     const val JPA_PLUGIN_VERSION = "1.9.25"
-    const val SPRING_BOOT_VERSION = "3.5.4"
-    const val SPRING_DEPENDENCY_MANAGEMENT_VERSION = "1.1.7"
+    const val SPRING_BOOT_VERSION = "3.3.5"
+    const val SPRING_DEPENDENCY_MANAGEMENT_VERSION = "1.1.6"
     const val KTLINT_VERSION = "11.5.1"
 }
