a5f339acc8e03c866c1e7cf1c47ff3ad01c54cf3
coehgns <modooboiroo@gmail.com>
2025-09-13 23:27:26 +0900

    chore(#149): Spring, Spring management version 수정

--- buildSrc/src/main/kotlin/DependencyVersions.kt
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -5,5 +5,5 @@ object DependencyVersions {
     const val QUERYDSL = "5.0.0"
     const val JAKARTA_PERSISTENCE = "3.1.0"
     const val JAKARTA_ANNOTATION = "2.1.1"
-    const val SWAGGER = "2.5.0"
+    const val SWAGGER = "2.0.2"
 }
