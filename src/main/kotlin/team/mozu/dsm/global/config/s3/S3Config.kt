08bbb4c461a6f0be4c847a984de16c0ed407a14a
박태수 <taesupark0504@gmail.com>
2025-09-26 18:25:56 +0900

    refactor: iam key추가

--- src/main/kotlin/team/mozu/dsm/global/config/s3/S3Config.kt
+++ src/main/kotlin/team/mozu/dsm/global/config/s3/S3Config.kt
@@ -3,19 +3,24 @@ package team.mozu.dsm.global.config.s3
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
 
 @Configuration
 class S3Config(
+    @Value("\${cloud.aws.credentials.access-key}") private val accessKey: String,
+    @Value("\${cloud.aws.credentials.secret-key}") private val secretKey: String,
     @Value("\${cloud.aws.region.static}") private val region: String
 ) {
 
     @Bean
-    fun s3Client(): S3Client =
-        S3Client.builder()
+    fun s3Client(): S3Client {
+        val creds = AwsBasicCredentials.create(accessKey, secretKey)
+        return S3Client.builder()
             .region(Region.of(region))
-            .credentialsProvider(DefaultCredentialsProvider.create())
+            .credentialsProvider(StaticCredentialsProvider.create(creds))
             .build()
+    }
 }
