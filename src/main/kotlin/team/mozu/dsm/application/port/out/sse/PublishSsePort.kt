b104c0a8867edf1f5c878ab3a21e4614afc0922f
jidohyun <shickcoong777@gmail.com>
2025-10-02 00:14:33 +0900

    refactor: remove old SSE infrastructure

--- src/main/kotlin/team/mozu/dsm/application/port/out/sse/PublishSsePort.kt
+++
@@ -1,8 +0,0 @@
-package team.mozu.dsm.application.port.out.sse
-
-interface PublishSsePort {
-
-    fun publishTo(clientId: String, eventName: String, data: Any)
-
-    fun publishToAll(eventName: String, data: Any)
-}
--- src/main/kotlin/team/mozu/dsm/application/port/out/sse/SubscribeSsePort.kt
+++
@@ -1,8 +0,0 @@
-package team.mozu.dsm.application.port.out.sse
-
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
-
-interface SubscribeSsePort {
-
-    fun subscribe(clientId: String): SseEmitter
-}
