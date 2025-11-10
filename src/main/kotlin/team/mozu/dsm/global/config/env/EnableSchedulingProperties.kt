47e0a053556ffb9b4dc4b70b2589254f99ac80a6
민수아 <Minsua0503@gmail.com>
2025-11-10 11:13:54 +0900

    refactor(#166) :: @EnableScheduling 설정을 별도 클래스로 분리

---
+++ src/main/kotlin/team/mozu/dsm/global/config/env/EnableSchedulingProperties.kt
@@ -0,0 +1,8 @@
+package team.mozu.dsm.global.config.env
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class EnableSchedulingProperties
