df06d0372de9ae736680f9ce21a3941b4fdbb40b
민수아 <Minsua0503@gmail.com>
2025-11-13 07:25:31 -0800

    feat(#166) :: 컬럼 제약 조건 추가

--- src/main/kotlin/team/mozu/dsm/adapter/out/team/entity/TeamJpaEntity.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/team/entity/TeamJpaEntity.kt
@@ -13,7 +13,7 @@ import team.mozu.dsm.global.entity.BaseTimeEntity
 @Table(name = "tbl_lesson_team")
 class TeamJpaEntity(
 
-    @Column(columnDefinition = "VARCHAR(100)")
+    @Column(nullable = false, unique = true, columnDefinition = "VARCHAR(100)")
     var teamName: String?,
 
     @Column(nullable = false, columnDefinition = "VARCHAR(100)")
