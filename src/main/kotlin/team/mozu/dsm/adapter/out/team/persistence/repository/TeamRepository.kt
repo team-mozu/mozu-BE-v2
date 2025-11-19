7e2df6ffcfb9d7a093611a255353a75c5771c5af
민수아 <Minsua0503@gmail.com>
2025-11-19 19:47:06 +0900

    chore(#168) :: 불필요한 쿼리 제거

--- src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/repository/TeamRepository.kt
+++ src/main/kotlin/team/mozu/dsm/adapter/out/team/persistence/repository/TeamRepository.kt
@@ -4,7 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import team.mozu.dsm.adapter.out.team.entity.TeamJpaEntity
 import java.util.UUID
 
-interface TeamRepository : JpaRepository<TeamJpaEntity, UUID> {
-
-    fun existsByTeamName(teamName: String): Boolean
-}
+interface TeamRepository : JpaRepository<TeamJpaEntity, UUID>
