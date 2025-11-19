a4fae85d6406d80fbc184f327b535a4b76f948c5
민수아 <Minsua0503@gmail.com>
2025-11-19 14:37:07 +0900

    refactor(#166) :: flyway 분리

--- src/main/resources/db/migration/V2__add_constraints_to_team_name.sql
+++ src/main/resources/db/migration/V2__add_constraints_to_team_name.sql
@@ -1,3 +1,2 @@
-ALTER TABLE tbl_lesson_team
-    MODIFY COLUMN team_name VARCHAR(100) NOT NULL,
-    ADD CONSTRAINT uq_team_name UNIQUE (team_name);
+ALTER TABLE tbl_lesson_team MODIFY COLUMN team_name VARCHAR(100) NOT NULL;
+ALTER TABLE tbl_lesson_team ADD CONSTRAINT uq_team_name UNIQUE (team_name);
