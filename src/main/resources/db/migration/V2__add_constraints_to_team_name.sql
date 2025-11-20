9bd947787c4925fbcb8a35226722b49f21c0b84e
민수아 <Minsua0503@gmail.com>
2025-11-20 10:11:33 +0900

    refactor(#168) :: 단일 유니크 복합 유니크로 변경

--- src/main/resources/db/migration/V2__add_constraints_to_team_name.sql
+++ src/main/resources/db/migration/V2__add_constraints_to_team_name.sql
@@ -1,2 +1,2 @@
 ALTER TABLE tbl_lesson_team MODIFY COLUMN team_name VARCHAR(100) NOT NULL;
-ALTER TABLE tbl_lesson_team ADD CONSTRAINT uq_team_name UNIQUE (team_name);
+ALTER TABLE tbl_lesson_team ADD CONSTRAINT uq_team_name_lesson_id UNIQUE (team_name, lesson_id);
